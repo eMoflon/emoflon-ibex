ef9393fc29fe13c13ea1f1f1aaa2f2ee02dc6317
LarsFritsche <arikae00@jesitec.de>
2016-12-20 03:19:37 +0100

    implemented registerMetamodels in new run files of CDToDoC

--- org.emoflon.ibex.tgg.tests/CDToDoc/src/org/emoflon/ibex/tgg/run/BACKWARD_ILP_App.java
+++ org.emoflon.ibex.tgg.tests/CDToDoc/src/org/emoflon/ibex/tgg/run/BACKWARD_ILP_App.java
@@ -57,30 +57,29 @@ public class BACKWARD_ILP_App {
 		
 	
 	private static void registerMetamodels(ResourceSet rs){
-		// Register internals
-		LanguagePackage.eINSTANCE.getName();
-		RuntimePackage.eINSTANCE.getName();
-
-		
-		// Add mapping for correspondence metamodel
-		Resource corr = rs.getResource(URI.createFileURI("model/CDToDoc.ecore"), true);
-		EPackage pcorr = (EPackage) corr.getContents().get(0);
-		Registry.INSTANCE.put(corr.getURI().toString(), corr);
-		Registry.INSTANCE.put("platform:/resource/CDToDoc/model/CDToDoc.ecore", pcorr);
-		Registry.INSTANCE.put("platform:/plugin/CDToDoc/model/CDToDoc.ecore", pcorr);
-		
-		// TODO: Uncomment the following lines and register source and target metamodels
-		// Add mappings for all other required dependencies
-		//Resource source = rs.getResource(URI.createFileURI("domains/MySource.ecore"), true);
-		//EPackage psource = (EPackage) source.getContents().get(0);
-		//Registry.INSTANCE.put(source.getURI().toString(), psource);
-		//Registry.INSTANCE.put("platform:/resource/CDToDoc/domains/MySource.ecore", psource);
-		//Registry.INSTANCE.put("platform:/plugin/MySource/model/MySource.ecore", psource);
-		
-		//Resource target = rs.getResource(URI.createFileURI("domains/MyTarget.ecore"), true);
-		//EPackage ptarget = (EPackage) target.getContents().get(0);
-		//Registry.INSTANCE.put(target.getURI().toString(), ptarget);
-		//Registry.INSTANCE.put("platform:/resource/CDToDoc/domains/MyTarget.ecore", ptarget);	
-		//Registry.INSTANCE.put("platform:/plugin/MyTarget/model/MyTarget.ecore", ptarget);	
+		 // Register internals
+		 LanguagePackage.eINSTANCE.getName();
+		 RuntimePackage.eINSTANCE.getName();
+		 
+		 
+		 // Add mapping for correspondence metamodel
+		 Resource corr = rs.getResource(URI.createFileURI("model/CDToDoc.ecore"), true);
+		 EPackage pcorr = (EPackage) corr.getContents().get(0);
+		 Registry.INSTANCE.put(corr.getURI().toString(), corr);
+		 Registry.INSTANCE.put("platform:/resource/CDToDoc/model/CDToDoc.ecore", pcorr);
+		 Registry.INSTANCE.put("platform:/plugin/CDToDoc/model/CDToDoc.ecore", pcorr);
+		 
+		 // Add mappings for all other required dependencies
+		 Resource cd = rs.getResource(URI.createFileURI("domains/CD.ecore"), true);
+		 EPackage pcd = (EPackage) cd.getContents().get(0);
+		 Registry.INSTANCE.put(cd.getURI().toString(), pcd);
+		 Registry.INSTANCE.put("platform:/resource/CDToDoc/domains/CD.ecore", pcd);
+		 Registry.INSTANCE.put("platform:/plugin/CD/model/CD.ecore", pcd);
+		 
+		 Resource doc = rs.getResource(URI.createFileURI("domains/Doc.ecore"), true);
+		 EPackage pdoc = (EPackage) doc.getContents().get(0);
+		 Registry.INSTANCE.put(doc.getURI().toString(), pdoc);
+		 Registry.INSTANCE.put("platform:/resource/CDToDoc/domains/Doc.ecore", pdoc);	
+		 Registry.INSTANCE.put("platform:/plugin/Doc/model/Doc.ecore", pdoc);	
 	}
 }
