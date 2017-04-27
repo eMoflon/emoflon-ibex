a28223c3e09cd996d601dbdf2c07dddc9dc4fdbd
Florian Stolte <fstolte@mail.upb.de>
2017-04-27 14:19:35 +0200

    Refactoring and Javadoc for ConstraintPattern.

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/pattern/rulepart/support/ConstraintPattern.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/pattern/rulepart/support/ConstraintPattern.java
@@ -1,6 +1,7 @@
 package org.emoflon.ibex.tgg.compiler.pattern.rulepart.support;
 
 import org.emoflon.ibex.tgg.compiler.pattern.rulepart.RulePartPattern;
+import org.moflon.tgg.mosl.tgg.TripleGraphGrammarFile;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,26 +11,46 @@ import org.emoflon.ibex.tgg.compiler.PatternSuffixes;
 
 import language.*;
 
+/**
+ * The ConstraintPattern can be used to implement application constraints for a {@link TGGRule}.
+ * It extends {@link RulePartPattern} by allowing elements in the body and signature that are not part of the rule.
+ */
 public class ConstraintPattern extends RulePartPattern {
 	private Collection<TGGRuleElement> signatureElements = new ArrayList<>();
-	private Collection<TGGRuleElement> bodyElements = new ArrayList<>();
+	private Collection<TGGRuleNode> bodyNodes = new ArrayList<>();
+	private Collection<TGGRuleEdge> bodyEdges = new ArrayList<>();
 	
+	/**
+	 * Creates a new ConstraintPattern. The body and signature are initialized with the given parameters.
+	 * @param rule The {@link TGGRule} that this constraint belongs to.
+	 * @param signatureElements The {@link TGGElements} that shall be part of this constraint's signature.
+	 * @param bodyElements The {@link TGGElements} that shall be part of this constraint, but not part of the signature).
+	 */
 	public ConstraintPattern(TGGRule rule, Collection<TGGRuleElement> signatureElements, Collection<TGGRuleElement> bodyElements) {
 		this.rule = rule;
 		this.signatureElements = signatureElements;
-		this.bodyElements = bodyElements;
-		this.initialize();
 		
+		this.bodyNodes = bodyElements.stream()
+									 .filter(e -> e instanceof TGGRuleNode)
+									 .map(e -> (TGGRuleNode)e)
+									 .collect(Collectors.toSet());
+		
+		this.bodyEdges = bodyElements.stream()
+				 .filter(e -> e instanceof TGGRuleEdge)
+				 .map(e -> (TGGRuleEdge)e)
+				 .collect(Collectors.toSet());
+		
+		this.initialize();
 	}
 
 	@Override
 	protected boolean isRelevantForBody(TGGRuleEdge e) {
-		return this.bodyElements.contains(e);
+		return this.bodyEdges.contains(e);
 	}
 
 	@Override
 	protected boolean isRelevantForBody(TGGRuleNode n) {
-		return this.bodyElements.contains(n);
+		return this.bodyNodes.contains(n);
 	}
 
 	@Override
@@ -49,19 +70,13 @@ public class ConstraintPattern extends RulePartPattern {
 	
 	@Override
 	public Collection<TGGRuleNode> getBodyNodes() {
-		return this.bodyElements.stream()
-								.filter(e -> e instanceof TGGRuleNode)
-								.map(e -> (TGGRuleNode)e)
-								.collect(Collectors.toSet());
+		return this.bodyNodes;
 	}
 
 	
 	@Override
 	public Collection<TGGRuleEdge> getBodyEdges() {
-		return this.bodyElements.stream()
-				.filter(e -> e instanceof TGGRuleEdge)
-				.map(e -> (TGGRuleEdge)e)
-				.collect(Collectors.toSet());
+		return this.bodyEdges;
 	}
 
 	@Override
