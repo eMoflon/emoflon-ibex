f664d6d6b8788d5412ffd3238e6c5ce536ebd090
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-05-06 10:13:20 +0200

    Improved tests, added two more

--- org.emoflon.ibex.tgg.tests/testsuite1.familiestopersons/test/testsuite1/familiestopersons/TestAxiom.java
+++ org.emoflon.ibex.tgg.tests/testsuite1.familiestopersons/test/testsuite1/familiestopersons/TestAxiom.java
@@ -2,51 +2,32 @@ package testsuite1.familiestopersons;
 
 import java.io.IOException;
 
-import org.apache.log4j.BasicConfigurator;
-import org.benchmarx.emf.Comparator;
-import org.benchmarx.families.core.FamiliesComparator;
-import org.benchmarx.persons.core.PersonsComparator;
-import org.benchmarx.util.EMFUtil;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.emoflon.ibex.tgg.operational.strategies.gen.MODELGENStopCriterion;
-import org.emoflon.ibex.tgg.run.MODELGEN_App;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import Families.FamilyRegister;
-import Persons.PersonRegister;
-
-public class TestAxiom {
-	private MODELGEN_App generator;
-	private Comparator<FamilyRegister> familiesComp;
-	private Comparator<PersonRegister> personsComp;
+public class TestAxiom extends ModelGenTestCase {
 	
-	
-	@BeforeClass
-	public static void init() {
-		BasicConfigurator.configure();
-	}
-
-	@Before
-	public void createGenerator() throws IOException {
-		generator = new MODELGEN_App("testsuite1.familiestopersons", "./../", false);
-		familiesComp = new FamiliesComparator();
-		personsComp = new PersonsComparator();
+	@Test
+	public void testOneRoot() throws IOException {
+		stop.setMaxRuleCount("HandleRegisters", 1);
+		stop.setMaxRuleCount("IgnoreFamily", 0);
+		runGenerator(stop);
+		assertPostcondition("singleFamilyReg", "singlePersonReg");
 	}
 
 	@Test
-	public void testOneRoot() throws IOException {
-		MODELGENStopCriterion stop = new MODELGENStopCriterion(generator.getTGG());
+	public void testOneRootAndTwoFamilies() throws IOException {
 		stop.setMaxRuleCount("HandleRegisters", 1);
-		generator.setStopCriterion(stop); 
-		generator.run();
-		generator.terminate();
-
-		Resource srcExp = EMFUtil.loadExpectedResource("singleFamilyReg", generator.getResourceSet());
-		Resource trgExp = EMFUtil.loadExpectedResource("singlePersonReg", generator.getResourceSet());
-		familiesComp.assertEquals((FamilyRegister)srcExp.getContents().get(0), (FamilyRegister)generator.getSourceResource().getContents().get(0));
-		personsComp.assertEquals((PersonRegister)trgExp.getContents().get(0), (PersonRegister)generator.getTargetResource().getContents().get(0));
+		stop.setMaxRuleCount("IgnoreFamily", 2);
+		runGenerator(stop);
+		assertPostcondition("singleFamilyRegAndTwoFamilies", "singlePersonReg");
 	}
-
+	
+	@Test
+	public void testMultipleRoots() throws IOException {
+		stop.setMaxRuleCount("HandleRegisters", 3);
+		stop.setMaxRuleCount("IgnoreFamily", 0);
+		runGenerator(stop);
+		generator.saveModels();
+		assertPostcondition("multipleFamilyReg", "multiplePersonReg");
+	}
 }
