a171bca8eb2d99044f3cb50f3fa2f5d5e01a8e59
Florian Stolte <fstolte@mail.upb.de>
2017-05-09 12:13:27 +0200

    Added two more tests. Created an Ibex.jar for testing without depending on a dev workspace.

--- org.emoflon.ibex.tgg.tests/testsuite1_familiestopersons/test/testsuite1/familiestopersons/TestComplexFamilies.java
+++ org.emoflon.ibex.tgg.tests/testsuite1_familiestopersons/test/testsuite1/familiestopersons/TestComplexFamilies.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestComplexFamilies extends ModelGenTestCase {
 	
 	@Test
-	public void testFamilyWithThreeSonsAndThreeDaughters() throws IOException {
+	public void testFamilyWithSixChildren() throws IOException {
 		stop.setMaxRuleCount("HandleRegisters", 1);
 		stop.setMaxRuleCount("IgnoreFamily", 1);
 		stop.setMaxRuleCount("FatherToMale", 1);
@@ -16,6 +16,30 @@ public class TestComplexFamilies extends ModelGenTestCase {
 		stop.setMaxRuleCount("SonToMale", 3);
 		runGenerator(stop);
 		assertPostcondition("sixChildFamily", "fourFemaleFourMalePersons");
+	}
+
+	@Test
+	public void testFourFamiliesWithSixteenChildren() throws IOException {
+		stop.setMaxRuleCount("HandleRegisters", 1);
+		stop.setMaxRuleCount("IgnoreFamily", 1);
+		stop.setMaxRuleCount("FatherToMale", 1);
+		stop.setMaxRuleCount("MotherToFemale", 1);
+		stop.setMaxRuleCount("DaughterToFemale", 8);
+		stop.setMaxRuleCount("SonToMale", 8);
+		runGenerator(stop);
+		assertPostcondition("sixteenChildFamily", "eightFemaleEightMalePersons");
+	}
+
+	@Test
+	public void testFourFamiliesWithSixtyChildren() throws IOException {
+		stop.setMaxRuleCount("HandleRegisters", 1);
+		stop.setMaxRuleCount("IgnoreFamily", 1);
+		stop.setMaxRuleCount("FatherToMale", 1);
+		stop.setMaxRuleCount("MotherToFemale", 1);
+		stop.setMaxRuleCount("DaughterToFemale", 30);
+		stop.setMaxRuleCount("SonToMale", 30);
+		runGenerator(stop);
+		assertPostcondition("sixtyChildFamily", "thirtyFemaleThirtyMalePersons");
 	}
 
 }
