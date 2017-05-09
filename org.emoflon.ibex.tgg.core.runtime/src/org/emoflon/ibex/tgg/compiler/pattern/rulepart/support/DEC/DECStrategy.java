34bbe07a00075f176140e459cc24f7412af6a15b
ErhanLeblebici <erhan.leblebici@es.tu-darmstadt.de>
2017-05-09 12:37:35 +0200

    cf. #139

---
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/pattern/rulepart/support/DEC/DECStrategy.java
@@ -0,0 +1,15 @@
+package org.emoflon.ibex.tgg.compiler.pattern.rulepart.support.DEC;
+
+public enum DECStrategy {
+
+	// NONE = No checking of DECs at all
+	NONE,
+
+	// Filter NACs for the case where no other rule would "potentially" translate a dangling edge.
+	// This corresponds to the old implementation (with SDMs) in eMoflon
+	FILTER_NACS,
+
+	// Filter NACs and, additionally, PACs so that the potential rules must really have a source/target
+	// match for the edge
+	FILTER_NACS_AND_PACS
+}
