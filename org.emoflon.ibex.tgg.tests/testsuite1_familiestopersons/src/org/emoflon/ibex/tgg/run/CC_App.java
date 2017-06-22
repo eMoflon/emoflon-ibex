438a6333940b160b34ce39545eb9a81c537f1b33
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-06-22 11:00:21 +0200

    Corrected order of MM registration

--- org.emoflon.ibex.tgg.tests/testsuite1_familiestopersons/src/org/emoflon/ibex/tgg/run/CC_App.java
+++ org.emoflon.ibex.tgg.tests/testsuite1_familiestopersons/src/org/emoflon/ibex/tgg/run/CC_App.java
@@ -37,11 +37,10 @@ public class CC_App extends CC {
 		}
 
 	@Override
-	protected void registerUserMetamodels() throws IOException {
-		loadAndRegisterMetamodel(projectPath + "/model/" + projectPath + ".ecore");
-		
+	protected void registerUserMetamodels() throws IOException {		
 		rs.getPackageRegistry().put("platform:/resource/SimpleFamilies/model/SimpleFamilies.ecore", SimpleFamiliesPackageImpl.init());
 		rs.getPackageRegistry().put("platform:/resource/SimplePersons/model/SimplePersons.ecore", SimplePersonsPackageImpl.init());
+		loadAndRegisterMetamodel(projectPath + "/model/" + projectPath + ".ecore");
 	}
 }
 
