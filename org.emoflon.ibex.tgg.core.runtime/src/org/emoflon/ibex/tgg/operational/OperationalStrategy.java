b5e119fcb17774114d69f9fd5365c8d4efd00450
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-09-08 23:44:14 +0200

    Switched to random update policy for better default behaviour

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/OperationalStrategy.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/OperationalStrategy.java
@@ -26,7 +26,7 @@ import org.emoflon.ibex.tgg.operational.util.IMatch;
 import org.emoflon.ibex.tgg.operational.util.IbexOptions;
 import org.emoflon.ibex.tgg.operational.util.ManipulationUtil;
 import org.emoflon.ibex.tgg.operational.util.MatchContainer;
-import org.emoflon.ibex.tgg.operational.util.NextMatchUpdatePolicy;
+import org.emoflon.ibex.tgg.operational.util.RandomMatchUpdatePolicy;
 import org.emoflon.ibex.tgg.operational.util.RuleInfos;
 import org.emoflon.ibex.tgg.operational.util.UpdatePolicy;
 
@@ -74,7 +74,7 @@ public abstract class OperationalStrategy {
 	private boolean domainsHaveNoSharedTypes;
 
 	public OperationalStrategy(String projectPath, String workspacePath, boolean flatten, boolean debug) {
-		this(projectPath, workspacePath, flatten, debug, new NextMatchUpdatePolicy());
+		this(projectPath, workspacePath, flatten, debug, new RandomMatchUpdatePolicy());
 	}
 
 	public OperationalStrategy(String projectPath, String workspacePath, boolean flatten, boolean debug, UpdatePolicy policy) {
