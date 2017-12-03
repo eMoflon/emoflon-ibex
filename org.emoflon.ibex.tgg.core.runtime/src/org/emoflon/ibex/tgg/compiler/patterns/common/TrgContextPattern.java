901bc03ad86a15261b0bcb31ff2a1a585476196f
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-12-03 20:12:42 +0100

    Trg and TrgContext now use new basis pattern

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/patterns/common/TrgContextPattern.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/patterns/common/TrgContextPattern.java
@@ -1,42 +1,56 @@
 package org.emoflon.ibex.tgg.compiler.patterns.common;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.emoflon.ibex.tgg.compiler.patterns.PatternFactory;
 import org.emoflon.ibex.tgg.compiler.patterns.PatternSuffixes;
 
 import language.BindingType;
 import language.DomainType;
+import language.TGGRule;
 import language.TGGRuleEdge;
 import language.TGGRuleNode;
 
-public class TrgContextPattern extends IbexPattern {
+public class TrgContextPattern extends IbexBasePattern {
 
 	public TrgContextPattern(PatternFactory factory) {
-		super(factory.getRule());
+		initialise(factory.getRule());
+		createPatternNetwork(factory);
+	}
+	
+	protected void initialise(TGGRule rule) {
+		String name = rule.getName() + PatternSuffixes.TRG_CONTEXT;
+		
+		Collection<TGGRuleNode> signatureNodes = rule.getNodes().stream()
+					.filter(this::isSignatureNode)
+					.collect(Collectors.toList());
+		
+		Collection<TGGRuleEdge> localEdges = rule.getEdges().stream()
+					.filter(this::isLocalEdge)
+					.collect(Collectors.toList());
+		
+		Collection<TGGRuleNode> localNodes = Collections.emptyList();
+		
+		super.initialise(name, signatureNodes, localNodes, localEdges);
 	}
 
-	@Override
-	public boolean isRelevantForSignature(TGGRuleNode e) {
+	private boolean isSignatureNode(TGGRuleNode n) {
+		return n.getDomainType() == DomainType.TRG && n.getBindingType() == BindingType.CONTEXT;
+	}
+
+	protected boolean isLocalEdge(TGGRuleEdge e) {
 		return e.getDomainType() == DomainType.TRG && e.getBindingType() == BindingType.CONTEXT;
 	}
 
-	@Override
-	protected boolean isRelevantForBody(TGGRuleEdge e) {
-		return e.getDomainType() == DomainType.TRG && e.getBindingType() == BindingType.CONTEXT;
+	private void createPatternNetwork(PatternFactory factory) {
+		// Leaf pattern
 	}
-
-	@Override
-	protected String getPatternNameSuffix() {
-		return PatternSuffixes.TRG_CONTEXT;
-	}
-
-	@Override
-	protected boolean isRelevantForBody(TGGRuleNode n) {
-		return isRelevantForSignature(n);
-	}
-
+		
 	@Override
 	protected boolean injectivityIsAlreadyChecked(TGGRuleNode node1, TGGRuleNode node2) {
+		// Leaf pattern so we have to check injectivity here
 		return false;
 	}
-
 }
--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/patterns/common/TrgPattern.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/patterns/common/TrgPattern.java
@@ -1,44 +1,56 @@
 package org.emoflon.ibex.tgg.compiler.patterns.common;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.emoflon.ibex.tgg.compiler.patterns.PatternFactory;
 import org.emoflon.ibex.tgg.compiler.patterns.PatternSuffixes;
 
 import language.BindingType;
 import language.DomainType;
+import language.TGGRule;
 import language.TGGRuleEdge;
 import language.TGGRuleNode;
 
-public class TrgPattern extends IbexPattern {
+public class TrgPattern extends IbexBasePattern {
 
 	public TrgPattern(PatternFactory factory) {
-		super(factory.getRule());
+		initialise(factory.getRule());
+		createPatternNetwork(factory);
+	}
+	
+	protected void initialise(TGGRule rule) {
+		String name = rule.getName() + PatternSuffixes.TRG;
 		
-		// Create pattern network
-		addTGGPositiveInvocation(factory.create(TrgContextPattern.class));
+		Collection<TGGRuleNode> signatureNodes = rule.getNodes().stream()
+					   .filter(this::isSignatureNode)
+					   .collect(Collectors.toList());
+		
+		Collection<TGGRuleEdge> localEdges = rule.getEdges().stream()
+				   .filter(this::isLocalEdge)
+				   .collect(Collectors.toList());
+		
+		Collection<TGGRuleNode> localNodes = Collections.emptyList();
+		
+		super.initialise(name, signatureNodes, localNodes, localEdges);
 	}
 
-	@Override
-	public boolean isRelevantForSignature(TGGRuleNode e) {
+	private boolean isSignatureNode(TGGRuleNode e) {
 		return e.getDomainType() == DomainType.TRG;
 	}
 
-	@Override
-	protected boolean isRelevantForBody(TGGRuleEdge e) {
+	private boolean isLocalEdge(TGGRuleEdge e) {
 		return e.getDomainType() == DomainType.TRG && e.getBindingType() == BindingType.CREATE;
 	}
-
-	@Override
-	protected String getPatternNameSuffix() {
-		return PatternSuffixes.TRG;
+	
+	private void createPatternNetwork(PatternFactory factory) {
+		addPositiveInvocation(factory.create(TrgContextPattern.class));
 	}
-
-	@Override
-	protected boolean isRelevantForBody(TGGRuleNode n) {
-		return isRelevantForSignature(n) && n.getBindingType() == BindingType.CREATE;
-	}
-
+	
 	@Override
 	protected boolean injectivityIsAlreadyChecked(TGGRuleNode node1, TGGRuleNode node2) {
+		// If both nodes are context then this has been checked in TrgContext
 		return node1.getBindingType() == BindingType.CONTEXT && node2.getBindingType() == BindingType.CONTEXT;
 	}
 
