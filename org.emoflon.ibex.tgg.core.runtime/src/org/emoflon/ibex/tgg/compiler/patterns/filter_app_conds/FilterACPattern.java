e9b6823e638fe1b84b8e83348cb1250508545b3d
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-12-04 16:58:11 +0100

    Refactored filter patterns

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/patterns/filter_app_conds/FilterACPattern.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/patterns/filter_app_conds/FilterACPattern.java
@@ -1,61 +1,60 @@
 package org.emoflon.ibex.tgg.compiler.patterns.filter_app_conds;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.tgg.compiler.patterns.PatternFactory;
 import org.emoflon.ibex.tgg.compiler.patterns.PatternSuffixes;
 import org.emoflon.ibex.tgg.compiler.patterns.common.IPattern;
-import org.emoflon.ibex.tgg.compiler.patterns.common.IbexPattern;
+import org.emoflon.ibex.tgg.compiler.patterns.common.IbexBasePattern;
 
 import language.TGGRuleEdge;
 import language.TGGRuleNode;
 
-public class FilterACPattern extends IbexPattern {
+public class FilterACPattern extends IbexBasePattern {
 	private TGGRuleNode entryPoint;
 	private EReference edgeType;
 	private EdgeDirection eDirection;
 	private IPattern premise;
 
 	public FilterACPattern(TGGRuleNode entryPoint, EReference edgeType, EdgeDirection eDirection, PatternFactory factory) {
-		super(factory.getFlattenedVersionOfRule());
 		this.entryPoint = entryPoint;
 		this.edgeType = edgeType;
 		this.eDirection = eDirection;
 
-		premise = factory.createSearchEdgePattern(entryPoint, edgeType, eDirection);
-		addDECAsBodyNode();
-		
-		// Create pattern network
-		addTGGPositiveInvocation(premise);
+		initialise(factory);
+		createPatternNetwork();
 	}
 	
-	private void addDECAsBodyNode() {
-		getLocalNodes().add(EcoreUtil.copy(FilterACHelper.getDECNode(((IbexPattern) premise).getRule())));
+	private void initialise(PatternFactory factory) {
+		String name = factory.getRule().getName() + getPatternNameSuffix(entryPoint, edgeType, eDirection);
+		premise = factory.createSearchEdgePattern(entryPoint, edgeType, eDirection);
+		
+		Collection<TGGRuleNode> signatureNodes = factory.getFlattenedVersionOfRule().getNodes().stream()
+				   .filter(this::notInPremise)
+				   .collect(Collectors.toList());
+	
+		Collection<TGGRuleEdge> localEdges = Collections.emptyList();
+	
+		Collection<TGGRuleNode> localNodes = Arrays.asList(FilterACHelper.getDECNode(premise));
+		
+		super.initialise(name, signatureNodes, localNodes, localEdges);		
+	}
+
+	private void createPatternNetwork() {
+		addPositiveInvocation(premise);
 	}
 
 	@Override
 	protected boolean injectivityIsAlreadyChecked(TGGRuleNode node1, TGGRuleNode node2) {
 		return true;
 	}
-
-	@Override
-	protected boolean isRelevantForBody(TGGRuleEdge e) {
-		return false;
-	}
-
-	@Override
-	protected boolean isRelevantForBody(TGGRuleNode n) {
-		return FilterACHelper.isDECNode(n);
-	}
 	
-	@Override
-	public boolean isRelevantForSignature(TGGRuleNode e) {
+	public boolean notInPremise(TGGRuleNode e) {
 		return premise.getSignatureNodes().stream().filter(element -> element.getName().equals(e.getName())).count() != 0;
-	}
-
-	@Override
-	protected String getPatternNameSuffix() {
-		return getPatternNameSuffix(entryPoint, edgeType, eDirection);
 	}
 	
 	public static String getPatternNameSuffix(TGGRuleNode entryPoint, EReference edgeType, EdgeDirection eDirection){
