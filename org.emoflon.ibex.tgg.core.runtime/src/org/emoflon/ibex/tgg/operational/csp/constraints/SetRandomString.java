f9f664c148ea1ec45940bb87019091fa6038bcfd
smilicas <milica@mail.uni-paderborn.de>
2018-01-05 14:32:27 +0100

    setRandomString att constraint added

---
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/csp/constraints/SetRandomString.java
@@ -0,0 +1,33 @@
+package org.emoflon.ibex.tgg.operational.csp.constraints;
+
+import org.apache.log4j.Logger;
+import org.emoflon.ibex.tgg.operational.csp.RuntimeTGGAttributeConstraint;
+import org.emoflon.ibex.tgg.operational.csp.RuntimeTGGAttributeConstraintVariable;
+
+public class SetRandomString extends RuntimeTGGAttributeConstraint {
+
+	Logger logger = Logger.getLogger(SetRandomString.class);
+
+	@Override
+	public void solve() {
+		if (variables.size() != 1)
+			throw new RuntimeException("The CSP -STRING_TO_INT- needs exactly one variables");
+
+		RuntimeTGGAttributeConstraintVariable v0 = variables.get(0);
+
+		String bindingStates = getBindingStates(v0);
+
+		if (bindingStates.equals("B")) {
+				setSatisfied(false);
+				
+		} else if (bindingStates.equals("F")) {
+			v0.bindToValue(generateValue(v0.getType()));
+			setSatisfied(true);
+		}
+		 else {
+				throw new UnsupportedOperationException(
+						"This case in the constraint has not been implemented yet: " + bindingStates);
+			}
+	}
+
+}
