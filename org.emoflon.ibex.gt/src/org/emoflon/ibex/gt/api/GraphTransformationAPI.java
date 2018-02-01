4f1602d600112876265d616ad57be566aae688d9
Patrick Robrecht <mail@patrick-robrecht.de>
2018-02-01 17:30:41 +0100

    Engine loads rules from .gt files

--- org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/api/GraphTransformationAPI.java
+++ org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/api/GraphTransformationAPI.java
@@ -9,11 +9,21 @@ import org.emoflon.ibex.gt.engine.GTEngine;
  * This abstract API is the super class for all concrete APIs generated for a
  * set of models.
  * 
+ * Concrete Implementations must implement loading rules from .gt files into the
+ * engine.
+ * 
  * @author Patrick Robrecht
  * @version 0.1
  */
 public abstract class GraphTransformationAPI {
+	/**
+	 * The engine to use for queries and transformations.
+	 */
 	protected GTEngine engine;
+
+	/**
+	 * The resource set containing the model file.
+	 */
 	protected ResourceSet model;
 
 	/**
@@ -26,10 +36,24 @@ public abstract class GraphTransformationAPI {
 	 *            the resource set containing the model file
 	 */
 	public GraphTransformationAPI(final GTEngine engine, final ResourceSet model) {
-		this.engine = engine;
-		this.model = model;
+		this.initialize(engine, model, false);
 	}
-	
+
+	/**
+	 * Creates a new GraphTransformationAPI with the given engine for the given
+	 * resource set.
+	 * 
+	 * @param engine
+	 *            the engine to use for queries and transformations
+	 * @param model
+	 *            the resource set containing the model file
+	 * @param debug
+	 *            <code>true</code> enables debugging, <code>false</code> to disable
+	 */
+	public GraphTransformationAPI(final GTEngine engine, final ResourceSet model, final boolean debug) {
+		this.initialize(engine, model, debug);
+	}
+
 	/**
 	 * Creates a new GraphTransformationAPI with the given engine for the given
 	 * model file.
@@ -37,13 +61,37 @@ public abstract class GraphTransformationAPI {
 	 * @param engine
 	 *            the engine to use for queries and transformations
 	 * @param filePath
-	 *            the resource set containing the model file
+	 *            the path to the model file
 	 */
 	public GraphTransformationAPI(final GTEngine engine, final String filePath) {
+		this.initialize(engine, filePath, false);
+	}
+
+	/**
+	 * Creates a new GraphTransformationAPI with the given engine for the given
+	 * model file.
+	 * 
+	 * @param engine
+	 *            the engine to use for queries and transformations
+	 * @param filePath
+	 *            the path to the model file
+	 * @param debug
+	 *            <code>true</code> enables debugging, <code>false</code> to disable
+	 */
+	public GraphTransformationAPI(final GTEngine engine, final String filePath, final boolean debug) {
+		this.initialize(engine, filePath, debug);
+	}
+
+	private void initialize(final GTEngine engine, final ResourceSet model, final boolean debug) {
 		this.engine = engine;
+		this.engine.setDebug(debug);
+		this.model = model;
+	}
+
+	private void initialize(final GTEngine engine, final String filePath, final boolean debug) {
 		ResourceSetImpl model = new ResourceSetImpl();
 		model.createResource(URI.createURI(filePath));
-		this.model = model;
+		this.initialize(engine, model, debug);
 	}
 
 	/**
--- org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GTEngine.java
+++ org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GTEngine.java
@@ -1,5 +1,18 @@
 package org.emoflon.ibex.gt.engine;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.emoflon.ibex.gt.editor.GTStandaloneSetup;
+import org.emoflon.ibex.gt.editor.gT.GraphTransformationFile;
+
+import com.google.inject.Injector;
+
 import IBeXLanguage.IBeXPattern;
 
 /**
@@ -10,6 +23,42 @@ import IBeXLanguage.IBeXPattern;
  * @version 0.1
  */
 public abstract class GTEngine {
+	protected boolean debug = false;
+
+	/**
+	 * Enables or disables the debugging mode.
+	 * 
+	 * @param debug
+	 *            the new value for the debugging flag
+	 */
+	public void setDebug(final boolean debug) {
+		this.debug = debug;
+	}
+
+	/**
+	 * Loads rules from an .gt file.
+	 * 
+	 * @param filePath
+	 *            the path to the given file.
+	 */
+	public void loadRulesFromFile(final String filePath) {
+		new org.eclipse.emf.mwe.utils.StandaloneSetup().setPlatformUri("../");
+		Injector injector = new GTStandaloneSetup().createInjectorAndDoEMFRegistration();
+		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+		Resource resource = resourceSet.createResource(URI.createURI("dummy:/" + filePath));
+		try {
+			InputStream in = new FileInputStream(filePath);
+			resource.load(in, resourceSet.getLoadOptions());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		GraphTransformationFile model = (GraphTransformationFile) resource.getContents().get(0);
+		if (this.debug) {
+			model.getRules().forEach(rule -> System.out.println(rule.getName()));
+		}
+	}
+
 	/**
 	 * Adds the IBeXPattern to the patterns of the engine.
 	 * 
