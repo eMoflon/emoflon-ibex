6d2843f24e886fd2b94f25342368de7c05b84951
Patrick Robrecht <mail@patrick-robrecht.de>
2018-02-01 19:45:48 +0100

    Transformation of parsed rules into internal model (problems with references to EClasses and all references)

--- org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GTEngine.java
+++ org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GTEngine.java
@@ -1,5 +1,6 @@
 package org.emoflon.ibex.gt.engine;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,9 +11,12 @@ import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.emoflon.ibex.gt.editor.GTStandaloneSetup;
 import org.emoflon.ibex.gt.editor.gT.GraphTransformationFile;
+import org.emoflon.ibex.gt.transformation.EditorToInternalGT;
+import org.emoflon.ibex.gt.utils.ModelPersistenceUtils;
 
 import com.google.inject.Injector;
 
+import GTLanguage.GTRuleSet;
 import IBeXLanguage.IBeXPattern;
 
 /**
@@ -42,20 +46,34 @@ public abstract class GTEngine {
 	 *            the path to the given file.
 	 */
 	public void loadRulesFromFile(final String filePath) {
+		File file = new File(filePath);
+		if (!file.exists()) {
+			throw new IllegalArgumentException(String.format("File %s does not exist", file.getAbsolutePath()));
+		}
+		if (!file.getName().endsWith(".gt")) {
+			throw new IllegalArgumentException(String.format("File %s does not have extension .gt", file.getName()));
+		}
+
+		// Prepare parsing gt file with Xtext parser.
 		new org.eclipse.emf.mwe.utils.StandaloneSetup().setPlatformUri("../");
 		Injector injector = new GTStandaloneSetup().createInjectorAndDoEMFRegistration();
 		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 		Resource resource = resourceSet.createResource(URI.createURI("dummy:/" + filePath));
+
+		// Parse .gt file.
 		try {
-			InputStream in = new FileInputStream(filePath);
+			InputStream in = new FileInputStream(file);
 			resource.load(in, resourceSet.getLoadOptions());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		GraphTransformationFile model = (GraphTransformationFile) resource.getContents().get(0);
+
+		// Transform into internal GT model.
+		GTRuleSet gtRuleSet = EditorToInternalGT.transformRuleSet(model);
 		if (this.debug) {
-			model.getRules().forEach(rule -> System.out.println(rule.getName()));
+			ModelPersistenceUtils.saveModel(gtRuleSet, "debug/gt/" + file.getName());
 		}
 	}
 
