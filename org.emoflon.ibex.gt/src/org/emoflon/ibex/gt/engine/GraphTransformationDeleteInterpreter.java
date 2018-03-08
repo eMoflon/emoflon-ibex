52f4ab4becfd519781c31b54b374ba9344a79484
Patrick Robrecht <mail@patrick-robrecht.de>
2018-03-08 17:37:21 +0100

    Add DPO check (only tested for containment edges)

--- org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GraphTransformationDeleteInterpreter.java
+++ org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GraphTransformationDeleteInterpreter.java
@@ -1,9 +1,13 @@
 package org.emoflon.ibex.gt.engine;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EContentsEList;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.common.operational.IDeletePatternInterpreter;
 import org.emoflon.ibex.common.operational.IMatch;
@@ -11,6 +15,8 @@ import org.emoflon.ibex.common.operational.PushoutSemantics;
 import org.emoflon.ibex.common.utils.EMFManipulationUtils;
 
 import IBeXLanguage.IBeXDeletePattern;
+import IBeXLanguage.IBeXEdge;
+import IBeXLanguage.IBeXNode;
 
 /**
  * Interpreter applying deletion of elements for graph transformation.
@@ -32,7 +38,7 @@ public class GraphTransformationDeleteInterpreter implements IDeletePatternInter
 	public Optional<IMatch> apply(final IBeXDeletePattern deletePattern, final IMatch match,
 			final PushoutSemantics po) {
 		// Check applicability with DPO semantics.
-		if (po == PushoutSemantics.DPO && this.hasDanglingEdges(deletePattern, match)) {
+		if (po == PushoutSemantics.DPO && !this.isApplicableDPO(deletePattern, match)) {
 			return Optional.empty();
 		}
 
@@ -52,8 +58,80 @@ public class GraphTransformationDeleteInterpreter implements IDeletePatternInter
 		return Optional.of(match);
 	}
 
-	private boolean hasDanglingEdges(final IBeXDeletePattern deletePattern, final IMatch match) {
-		// TODO
+	/**
+	 * Checks whether the delete pattern can be applied to the given match according
+	 * to DPO semantics.
+	 * 
+	 * @param deletePattern
+	 *            the delete pattern
+	 * @param match
+	 *            the match
+	 * @return true if and only if all dangling edges must be explicitly deleted by
+	 *         the pattern.
+	 */
+	@SuppressWarnings("rawtypes")
+	private boolean isApplicableDPO(final IBeXDeletePattern deletePattern, final IMatch match) {
+		for (IBeXNode deletedNode : deletePattern.getDeletedNodes()) {
+			EObject eObject = (EObject) match.get(deletedNode.getName());
+
+			// If a containment reference exists, the delete pattern must delete it.
+			if (eObject.eContainer() != null) {
+				if (!patternDeletesEdge(deletePattern, match, eObject, eObject.eContainer(),
+						eObject.eContainmentFeature())) {
+					return false;
+				}
+			}
+
+			// All existing references must be deleted by the pattern.
+			for (EContentsEList.FeatureIterator featureIterator = (EContentsEList.FeatureIterator) eObject
+					.eCrossReferences().iterator(); featureIterator.hasNext();) {
+				EObject eObjectTarget = (EObject) featureIterator.next();
+				EReference eReference = (EReference) featureIterator.feature();
+				if (!patternDeletesEdge(deletePattern, match, eObject, eObjectTarget, eReference)) {
+					return false;
+				}
+			}
+
+			// Find references with target eObject.
+			Collection<Setting> crossReferences = EcoreUtil.UsageCrossReferencer.find(eObject,
+					this.trashResource.getResourceSet());
+			for (Setting crossReference : crossReferences) {
+				System.out.println(
+						"cross reference" + crossReference.getEObject() + "" + crossReference.getEStructuralFeature());
+			}
+		}
 		return true;
+	}
+
+	/**
+	 * Checks whether the edge between the given source and target object of the
+	 * given type is deleted by the delete pattern on the given match
+	 * 
+	 * @param deletePattern
+	 *            the delete pattern
+	 * @param match
+	 *            the match
+	 * @param source
+	 *            the source object of the edge
+	 * @param target
+	 *            the target object of the edge
+	 * @param type
+	 *            the type of the edge
+	 * @return <code>true</code> if and only if the pattern specifies to delete the
+	 *         given edge
+	 */
+	private static boolean patternDeletesEdge(final IBeXDeletePattern deletePattern, final IMatch match,
+			final EObject source, final EObject target, final EReference type) {
+		for (IBeXEdge deletedEdge : deletePattern.getDeletedEdges()) {
+			if (deletedEdge.getType().getName().equals(type.getName())) {
+				EObject deletedEdgeSource = (EObject) match.get(deletedEdge.getSourceNode().getName());
+				EObject deletedEdgeTarget = (EObject) match.get(deletedEdge.getTargetNode().getName());
+				if ((deletedEdgeSource.equals(source) && deletedEdgeTarget.equals(target))
+						|| (deletedEdgeSource.equals(target) && deletedEdgeTarget.equals(source))) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
