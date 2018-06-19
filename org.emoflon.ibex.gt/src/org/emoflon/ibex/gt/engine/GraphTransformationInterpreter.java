4386e889476f0a30ac39776645a60f1f7384d8f6
Patrick Robrecht <mail@patrick-robrecht.de>
2018-06-19 11:34:30 +0200

    Remove unused imports

--- org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GraphTransformationInterpreter.java
+++ org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GraphTransformationInterpreter.java
@@ -1,15 +1,12 @@
 package org.emoflon.ibex.gt.engine;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.eclipse.emf.common.util.URI;
