ed59ce2587d5d43ec591d99efadd4390c404e36c
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-07-24 16:42:21 +0200

    Improved error message

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/util/String2EPrimitive.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/util/String2EPrimitive.java
@@ -8,13 +8,13 @@ public class String2EPrimitive {
 	public static Object convertLiteral(String literal, EDataType type) {
 		if(type.equals(EcorePackage.Literals.ESTRING) || type.getInstanceClass().isAssignableFrom(String.class)) {
 			if(!(literal.startsWith("\"") && literal.endsWith("\""))) 
-				throw new RuntimeException("Trimming of the string did not work. Your string should start and end with \"");
+				throw new RuntimeException("Trimming of the string did not work. Your string: " + literal + " should start and end with \"");
 			return literal.substring(1, literal.length() - 1);
 		}
 		
 		if(type.equals(EcorePackage.Literals.ECHAR) ) {
 			if(!(literal.startsWith("\'") && literal.endsWith("\'"))) 
-				throw new RuntimeException("Trimming of the char did not work. Your string should start and end with \'");
+				throw new RuntimeException("Trimming of the char did not work. Your string: " + literal + " should start and end with \'");
 
 			return literal.length() < 2 ? '\0' : literal.charAt(1);
 		}
