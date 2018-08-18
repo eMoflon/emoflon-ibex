324d4dfd24d132159f7df35232054044087b508e
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-08-18 17:38:21 +0200

    Added factory for standard JDK collections

---
+++ org.emoflon.ibex.common/src/org/emoflon/ibex/common/collections/jdk/JDKCollectionFactory.java
@@ -0,0 +1,38 @@
+package org.emoflon.ibex.common.collections.jdk;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.emoflon.ibex.common.collections.CollectionFactory;
+import org.emoflon.ibex.common.collections.IntToObjectMap;
+import org.emoflon.ibex.common.emf.EMFEdge;
+
+public class JDKCollectionFactory extends CollectionFactory {
+	@Override
+	public <K, V> Map<K, V> createObjectToObjectLinkedHashMap() {
+		return new LinkedHashMap<>();
+	}
+
+	@Override
+	public <K, V> Map<K, V> createObjectToObjectHashMap() {
+		return new HashMap<>();
+	}
+
+	@Override
+	public Set<EMFEdge> createEMFEdgeHashSet() {
+		return new HashSet<>();
+	}
+
+	@Override
+	public <T> Set<T> createObjectSet() {
+		return new HashSet<>();
+	}
+
+	@Override
+	public <T> IntToObjectMap<T> createIntToObjectHashMap() {
+		return new JDKIntToObjectMap<>();
+	}
+}
---
+++ org.emoflon.ibex.common/src/org/emoflon/ibex/common/collections/jdk/JDKIntToObjectMap.java
@@ -0,0 +1,35 @@
+package org.emoflon.ibex.common.collections.jdk;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.emoflon.ibex.common.collections.IntToObjectMap;
+
+public class JDKIntToObjectMap<T> extends IntToObjectMap<T> {
+	private HashMap<Integer, T> internal = new HashMap<>();
+
+	@Override
+	public T get(int i) {
+		return internal.get(i);
+	}
+
+	@Override
+	public void put(int i, T o) {
+		internal.put(i, o);
+	}
+
+	@Override
+	public Collection<Integer> keySet() {
+		return internal.keySet();
+	}
+
+	@Override
+	public int size() {
+		return internal.size();
+	}
+
+	@Override
+	public Collection<T> values() {
+		return internal.values();
+	}
+}
