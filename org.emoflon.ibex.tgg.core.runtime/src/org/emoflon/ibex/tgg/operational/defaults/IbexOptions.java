a696a855dc760f43cc774003b157b14cbcd6e2d8
Nils Weidmann <nilsweid@mail.upb.de>
2018-09-27 08:31:14 +0200

    Reset configurations (priority, edge patterns)

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/defaults/IbexOptions.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/defaults/IbexOptions.java
@@ -39,7 +39,7 @@ public class IbexOptions {
 		workspacePath = "./../";
 		repairAttributes = true;
 		setIlpSolver(SupportedILPSolver.Sat4J);
-		useEdgePatterns = false;
+		useEdgePatterns = true;
 	}
 
 	public IbexOptions debug(boolean debug) {
