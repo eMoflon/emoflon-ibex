b0cad995198bd9f8ffcf954d054c696a7485683d
saman <sam.soltani@gmail.com>
2019-06-27 12:51:56 +0200

    enhancement of get protocols method

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/monitoring/IbexController.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/monitoring/IbexController.java
@@ -8,10 +8,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.tgg.operational.matches.IMatch;
 import org.emoflon.ibex.tgg.operational.matches.ImmutableMatchContainer;
 import org.emoflon.ibex.tgg.operational.monitoring.data.ProtocolStep;
@@ -72,23 +76,32 @@ public abstract class IbexController implements IbexObserver, IUpdatePolicy {
 
 	private List<ProtocolStep> getProtocols() {
 		int index = protocolsStepList.size();
-		HashSet<EObject> srcResourceList = getResourceItems(op.getSourceResource());
-		HashSet<EObject> trgResourceList = getResourceItems(op.getTargetResource());
-		HashSet<EObject> corrResourceList = getResourceItems(op.getCorrResource());
+		HashSet<EObject> srcResourceList = new HashSet<EObject>();
+		HashSet<EObject> trgResourceList = new HashSet<EObject>();
+		HashSet<EObject> corrResourceList = new HashSet<EObject>();
 
+		Resource p = op.getProtocolResource();
+		EList<EObject> protocols = p.getContents();
+		if (protocols.size() > 0) {
+			EList<EObject> items = protocols.get(protocols.size() - 1).eCrossReferences();
+			srcResourceList = getResourceItems(items, op.getSourceResource());
+			trgResourceList = getResourceItems(items, op.getTargetResource());
+			corrResourceList = getResourceItems(items, op.getCorrResource());
+		}
+	
 		ProtocolStep protocolStep = new ProtocolStep(index, srcResourceList, trgResourceList, corrResourceList);
 		protocolsStepList.add(protocolStep);
-
+		
 		return protocolsStepList;
 	}
 	
-	private HashSet<EObject> getResourceItems(Resource resource) {
+	private HashSet<EObject> getResourceItems(EList<EObject> items, Resource resource) {
 		HashSet<EObject> currentResourceList = new HashSet<EObject>();
 		
-		for (EObject ri: resource.getContents()) {
-			if (!resourceList.contains(ri)) {
-				resourceList.add(ri);
-				currentResourceList.add(ri);
+		for (EObject item: items) {
+			if (!resourceList.contains(item) && item.eResource().equals(resource)) {
+				currentResourceList.add(item);
+				resourceList.add(item);
 			}
 		}
 		
