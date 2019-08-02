4aa47c760f5ce5cd0d2f3be8b8ba9be2d0c6d974
saman <sam.soltani@gmail.com>
2019-08-02 12:37:50 +0200

    merge master with procotol

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/monitoring/IbexController.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/monitoring/IbexController.java
@@ -21,108 +21,109 @@ import org.emoflon.ibex.tgg.operational.updatepolicy.IUpdatePolicy;
 
 public abstract class IbexController implements IbexObserver, IUpdatePolicy {
 
-    private int step = 0;
-    private OperationalStrategy operationalStrategy;
-    private Set<EObject> resourceList = new HashSet<EObject>();
-    private List<ProtocolStep> protocolsStepList = new ArrayList<ProtocolStep>();
-    private Map<IMatch, VictoryMatch> matchMapping = new HashMap<>();
+	private int step = 0;
+	private OperationalStrategy operationalStrategy;
+	private Set<EObject> resourceList = new HashSet<EObject>();
+	private List<ProtocolStep> protocolsStepList = new ArrayList<ProtocolStep>();
+	private Map<IMatch, VictoryMatch> matchMapping = new HashMap<>();
 
-    public void register(OperationalStrategy pOperationalStrategy) {
-	operationalStrategy = pOperationalStrategy;
-	pOperationalStrategy.registerObserver(this);
-	pOperationalStrategy.setUpdatePolicy(this);
-    }
-
-    @Override
-    public final IMatch chooseOneMatch(ImmutableMatchContainer matchContainer) {
-
-	updateMatchMapping(matchContainer.getMatches());
-
-	return chooseOneMatch(new VictoryDataPackage(matchMapping.values(), getProtocols()));
-    }
-
-    public Collection<VictoryMatch> getMoreMatches(int amount) {
-
-	/*
-	 * TODO implement This method needs to provide the specified number of
-	 * additional matches. This method will be called by the UI when the user
-	 * requests additional matches beyond those that were given to the UI in the
-	 * method above. The structure of the map is the same as described above.
-	 */
-
-	return null;
-    }
-
-    private List<ProtocolStep> getProtocols() {
-	int index = protocolsStepList.size();
-	HashSet<EObject> srcResourceList = new HashSet<EObject>();
-	HashSet<EObject> trgResourceList = new HashSet<EObject>();
-	HashSet<EObject> corrResourceList = new HashSet<EObject>();
-
-	Resource p = operationalStrategy.getProtocolResource();
-	EList<EObject> protocols = p.getContents();
-	if (protocols.size() > 0) {
-	    EList<EObject> items = protocols.get(protocols.size() - 1).eCrossReferences();
-	    srcResourceList = getResourceItems(items, operationalStrategy.getSourceResource());
-	    trgResourceList = getResourceItems(items, operationalStrategy.getTargetResource());
-	    corrResourceList = getResourceItems(items, operationalStrategy.getCorrResource());
-	    ProtocolStep protocolStep = new ProtocolStep(index,
-		    new TGGObjectGraph(srcResourceList, trgResourceList, corrResourceList));
-	    protocolsStepList.add(protocolStep);
+	public void register(OperationalStrategy pOperationalStrategy) {
+		operationalStrategy = pOperationalStrategy;
+		pOperationalStrategy.registerObserver(this);
+		pOperationalStrategy.setUpdatePolicy(this);
 	}
 
-	return protocolsStepList;
-    }
+	@Override
+	public final IMatch chooseOneMatch(ImmutableMatchContainer matchContainer) {
 
-    private HashSet<EObject> getResourceItems(EList<EObject> items, Resource resource) {
-	HashSet<EObject> currentResourceList = new HashSet<EObject>();
+		updateMatchMapping(matchContainer.getMatches());
 
-	for (EObject item : items) {
-	    if (!resourceList.contains(item) && item.eResource().equals(resource)) {
-		currentResourceList.add(item);
-		resourceList.add(item);
-	    }
+		return chooseOneMatch(new VictoryDataPackage(matchMapping.values(), getProtocols()));
 	}
 
-	return currentResourceList;
-    }
+	public Collection<VictoryMatch> getMoreMatches(int amount) {
 
-    private void updateMatchMapping(Collection<IMatch> pMatches) {
+		/*
+		 * TODO implement This method needs to provide the specified number of
+		 * additional matches. This method will be called by the UI when the user
+		 * requests additional matches beyond those that were given to the UI in the
+		 * method above. The structure of the map is the same as described above.
+		 */
 
-	for (Iterator<IMatch> iterator = matchMapping.keySet().iterator(); iterator.hasNext();) {
-	    IMatch match = iterator.next();
-	    if (pMatches.contains(match))
-		continue;
-	    else if (operationalStrategy.getBlockedMatches().containsKey(match))
-		continue;
-	    else
-		iterator.remove();
+		return null;
 	}
 
-	VictoryMatch.startMatchCreation(step);
+	private List<ProtocolStep> getProtocols() {
+		int index = protocolsStepList.size();
+		HashSet<EObject> srcResourceList = new HashSet<EObject>();
+		HashSet<EObject> trgResourceList = new HashSet<EObject>();
+		HashSet<EObject> corrResourceList = new HashSet<EObject>();
 
-	for (IMatch match : pMatches)
-	    if (matchMapping.containsKey(match))
-		matchMapping.get(match).setBlockingReason(null);
-	    else
-		matchMapping.put(match, VictoryMatch.newMatch(match));
-
-	if (operationalStrategy.getBlockedMatches() != null)
-	    operationalStrategy.getBlockedMatches().forEach((match, reason) -> {
-		if (matchMapping.containsKey(match))
-		    matchMapping.get(match).setBlockingReason(reason);
-		else {
-		    VictoryMatch vMatch = VictoryMatch.newMatch(match);
-		    vMatch.setBlockingReason(reason);
-		    matchMapping.put(match, vMatch);
+		Resource p = operationalStrategy.getProtocolResource();
+		EList<EObject> protocols = p.getContents();
+		if (protocols.size() > 0) {
+			EList<EObject> items = protocols.get(protocols.size() - 1).eCrossReferences();
+			
+			srcResourceList = getResourceItems(items, operationalStrategy.getSourceResource());
+			trgResourceList = getResourceItems(items, operationalStrategy.getTargetResource());
+			corrResourceList = getResourceItems(items, operationalStrategy.getCorrResource());
+			ProtocolStep protocolStep = new ProtocolStep(index,
+					new TGGObjectGraph(srcResourceList, trgResourceList, corrResourceList));
+			protocolsStepList.add(protocolStep);
 		}
-	    });
 
-	VictoryMatch.finishMatchCreation();
-	step++;
-    }
+		return protocolsStepList;
+	}
 
-    public abstract IMatch chooseOneMatch(VictoryDataPackage pDataPackage);
+	private HashSet<EObject> getResourceItems(EList<EObject> items, Resource resource) {
+		HashSet<EObject> currentResourceList = new HashSet<EObject>();
 
-    protected abstract int getRequestedMatchCount();
+		for (EObject item : items) {
+			if (!resourceList.contains(item) && item.eResource().equals(resource)) {
+				currentResourceList.add(item);
+				resourceList.add(item);
+			}
+		}
+
+		return currentResourceList;
+	}
+
+	private void updateMatchMapping(Collection<IMatch> pMatches) {
+
+		for (Iterator<IMatch> iterator = matchMapping.keySet().iterator(); iterator.hasNext();) {
+			IMatch match = iterator.next();
+			if (pMatches.contains(match))
+				continue;
+			else if (operationalStrategy.getBlockedMatches().containsKey(match))
+				continue;
+			else
+				iterator.remove();
+		}
+
+		VictoryMatch.startMatchCreation(step);
+
+		for (IMatch match : pMatches)
+			if (matchMapping.containsKey(match))
+				matchMapping.get(match).setBlockingReason(null);
+			else
+				matchMapping.put(match, VictoryMatch.newMatch(match));
+
+		if (operationalStrategy.getBlockedMatches() != null)
+			operationalStrategy.getBlockedMatches().forEach((match, reason) -> {
+				if (matchMapping.containsKey(match))
+					matchMapping.get(match).setBlockingReason(reason);
+				else {
+					VictoryMatch vMatch = VictoryMatch.newMatch(match);
+					vMatch.setBlockingReason(reason);
+					matchMapping.put(match, vMatch);
+				}
+			});
+
+		VictoryMatch.finishMatchCreation();
+		step++;
+	}
+
+	public abstract IMatch chooseOneMatch(VictoryDataPackage pDataPackage);
+
+	protected abstract int getRequestedMatchCount();
 }
