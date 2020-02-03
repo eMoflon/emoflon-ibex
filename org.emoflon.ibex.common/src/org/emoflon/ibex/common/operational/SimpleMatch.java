ae9cd2333847cb24ce78bfded2366ef9b581057c
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2020-02-03 20:20:07 +0100

    Comment in needed code

--- org.emoflon.ibex.common/src/org/emoflon/ibex/common/operational/SimpleMatch.java
+++ org.emoflon.ibex.common/src/org/emoflon/ibex/common/operational/SimpleMatch.java
@@ -86,10 +86,11 @@ public class SimpleMatch implements IMatch {
 		return getHashCode();
 	}
 
-//	@Override
-//	public boolean equals(final Object object) {
-//		return object instanceof IMatch && isEqual((IMatch) object);
-//	}
+	@Override
+	public boolean equals(final Object object) {
+		// TODO adrianm, lfritsche: bad performance
+		return object instanceof IMatch && isEqual((IMatch) object);
+	}
 
 	@Override
 	public String toString() {
