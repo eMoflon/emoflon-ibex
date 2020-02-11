3366885aa87ca914d7d0c2d8ae3390d570946e19
SebastianE <sebastian.ehmes@gmail.com>
2020-02-11 14:52:27 +0100

    minor refactoring

--- org.emoflon.ibex.common/src/org/emoflon/ibex/common/operational/SimpleMatch.java
+++ org.emoflon.ibex.common/src/org/emoflon/ibex/common/operational/SimpleMatch.java
@@ -1,15 +1,8 @@
 package org.emoflon.ibex.common.operational;
 
-import static org.emoflon.ibex.common.collections.CollectionFactory.cfactory;
-
 import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
-
-import com.google.common.collect.Lists;
 
 /**
  * A simple implementation of {@link IMatch}.
@@ -36,7 +29,6 @@ public class SimpleMatch implements IMatch {
 	 */
 	public SimpleMatch(final String patternName) {
 		this.patternName = patternName;
-		//this.parameters = cfactory.createObjectToObjectHashMap();
 		this.parameters = new LinkedHashMap<>();
 	}
 
@@ -48,7 +40,6 @@ public class SimpleMatch implements IMatch {
 	 */
 	public SimpleMatch(final IMatch match) {
 		this.patternName = match.getPatternName();
-		//this.parameters = cfactory.createObjectToObjectHashMap();
 		this.parameters = new LinkedHashMap<>();
 		match.getParameterNames().forEach(parameterName -> {
 			this.parameters.put(parameterName, match.get(parameterName));
@@ -83,7 +74,7 @@ public class SimpleMatch implements IMatch {
 	@Override
 	public long getHashCode() {
 		if(!hashInit) {
-			hash = collectionToHash(parameters.values());
+			hash = HashUtil.collectionToHash(parameters.values());
 			hashInit = true;
 		}
 		return hash;
@@ -92,88 +83,6 @@ public class SimpleMatch implements IMatch {
 	@Override
 	public int hashCode() {
 		return (int)getHashCode();
-	}
-	
-	public static long collectionToHash(Collection<Object> objects) {
-		final long seed = "HierStehtEinTollerSeed17".hashCode();
-		List<Byte> bytes = new LinkedList<>();
-		for(Object object : objects) {
-			bytes.addAll(intToBytes(object.hashCode()));
-		}
-		byte[] byteArray = new byte[bytes.size()];
-		int idx = 0;
-		for(Byte b : bytes) {
-			byteArray[idx]=b;
-			idx++;
-		}
-		return murmur3_64(byteArray, seed);
-	}
-	
-	public static List<Byte> intToBytes(final long value) {
-		List<Byte> bytes = new LinkedList<>();
-		final long mask = 0x00000000000000ff;
-		for(int i = 0; i<8; i++) {
-			bytes.add((byte)((value & mask<<i*8)>>i*8));
-		}
-		return bytes;
-	}
-	
-	public static byte[] intToByteArray(final long value) {
-		byte[] b = new byte[8];
-		final long mask = 0x00000000000000ff;
-		for(int i = 0; i<8; i++) {
-			b[i]=((byte)((value & mask<<i*8)>>i*8));
-		}
-		return b;
-	}
-	
-	public static long murmur_64_scramble(long k) {
-	    k *= 0xcc9e2d51;
-	    k = (k << 15) | (k >> 17);
-	    k *= 0x1b873593;
-	    return k;
-	}
-	public static long murmur3_64(final byte[] data, long seed)
-	{
-		long h = seed;
-	    long k;
-	    int lastIdx=0;
-	    /* Read in groups of 8. */
-	    for (int i = 0; i<data.length >> 3; i++) {
-	        // Here is a source of differing results across endiannesses.
-	        // A swap here has no effects on hash properties though.
-	        k = arrayRangeToLong(data, i*8, i*8+7);
-	        h ^= murmur_64_scramble(k);
-	        h = (h << 13) | (h >> 19);
-	        h = h * 5 + 0xe6546b64;
-	        lastIdx = i*8+7;
-	    }
-	    /* Read the rest. */
-	    k = 0;
-	    for (int i = lastIdx+1; i<data.length; i++) {
-	        k <<= 8;
-	        k |= data[i];
-	    }
-	    // A swap is *not* necessary here because the preceeding loop already
-	    // places the low bytes in the low places according to whatever endianness
-	    // we use. Swaps only apply when the memory is copied in a chunk.
-	    h ^= murmur_64_scramble(k);
-	    /* Finalize. */
-		h ^= data.length;
-		h ^= h >> 16;
-		h *= 0x85ebca6b;
-		h ^= h >> 13;
-		h *= 0xc2b2ae35;
-		h ^= h >> 16;
-		return h;
-	}
-	
-	public static long arrayRangeToLong(final byte[] data, int start, int end) {
-		long num = 0;
-		for(int i = 0; i<end-start; i++) {
-			num |= ((long)data[i+start])<<i*8;
-		}
-		return num;
 	}
 
 	@Override
