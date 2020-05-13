7d97e077bf7216bea00f25fa8b9cbca941d58f1d
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2020-05-13 16:01:29 +0200

    Complete CRSHelper implementation

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/strategies/integrate/conflicts/resolution/util/CRSHelper.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/strategies/integrate/conflicts/resolution/util/CRSHelper.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.emoflon.ibex.tgg.operational.strategies.integrate.conflicts.Conflict;
 import org.emoflon.ibex.tgg.operational.strategies.integrate.conflicts.GeneralConflict;
 import org.emoflon.ibex.tgg.operational.strategies.integrate.conflicts.HierarchicalConflict;
+import org.emoflon.ibex.tgg.operational.strategies.integrate.conflicts.MatchConflict;
 import org.emoflon.ibex.tgg.operational.strategies.integrate.conflicts.RelatedConflict;
 import org.emoflon.ibex.tgg.operational.strategies.integrate.conflicts.resolution.ConflictResolutionStrategy;
 
@@ -16,20 +17,35 @@ public class CRSHelper {
 			Class<T> conflictClass, Class<? extends ConflictResolutionStrategy<T>> crsClass) {
 		List<ConflictResolutionStrategy<?>> crsList = new LinkedList<>();
 		if (conflict instanceof HierarchicalConflict) {
-			// TODO adrianm: implement
-		} else if (conflict instanceof RelatedConflict) {
-			// TODO adrianm: implement
-		} else if (conflict instanceof Conflict) {
-			if (conflictClass.isInstance(conflict)) {
-				try {
-					crsList.add(crsClass.getConstructor(conflictClass).newInstance(conflict));
-				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-						| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-					e.printStackTrace();
+			for (MatchConflict mConfl : ((HierarchicalConflict) conflict).getConflictDependency()) {
+				if(mConfl instanceof RelatedConflict) {
+					for (Conflict confl : ((RelatedConflict) mConfl).getRelatedConflicts()) {
+						forConflict(crsList, confl, conflictClass, crsClass);
+					}
+				} else if (mConfl instanceof Conflict) {
+					forConflict(crsList, mConfl, conflictClass, crsClass);
 				}
 			}
+		} else if (conflict instanceof RelatedConflict) {
+			for (Conflict confl : ((RelatedConflict) conflict).getRelatedConflicts()) {
+				forConflict(crsList, confl, conflictClass, crsClass);
+			}
+		} else if (conflict instanceof Conflict) {
+			forConflict(crsList, conflict, conflictClass, crsClass);
 		}
 		return crsList;
+	}
+
+	private static <T extends Conflict> void forConflict(List<ConflictResolutionStrategy<?>> crsList,
+			GeneralConflict conflict, Class<T> conflictClass, Class<? extends ConflictResolutionStrategy<T>> crsClass) {
+		if (conflictClass.isInstance(conflict)) {
+			try {
+				crsList.add(crsClass.getConstructor(conflictClass).newInstance(conflict));
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
