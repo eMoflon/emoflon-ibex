de8161835803bebde7a4d5e0127308c4730b3ea6
SebastianE <sebastian.ehmes@gmail.com>
2020-07-16 19:34:34 +0200

    removed duplicate method resulting from a merge conflict

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/matches/TGGMatchParameterOrderProvider.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/matches/TGGMatchParameterOrderProvider.java
@@ -32,10 +32,6 @@ public class TGGMatchParameterOrderProvider {
 		}
 		initialized = true;
 	}
-	
-	public static boolean isInitialized() {
-		return ruleName2params != null;
-	}
 
 	public static LinkedList<String> getParams(String ruleName) {
 		return ruleName2params.get(ruleName);
