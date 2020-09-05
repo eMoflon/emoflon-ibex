6c569238fab5f140bd4a6375dfbfcddd82881552
Julian Barthel <j.barthel@gmx.de>
2020-09-05 17:07:00 +0200

    Fixed another mapping problem with PACs and some minor bugs

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/transformations/patterns/ContextPatternTransformation.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/transformations/patterns/ContextPatternTransformation.java
@@ -125,6 +125,7 @@ public class ContextPatternTransformation {
 					createSRCPattern(rule);
 					if(options.invocation.useGreenCorrPattern() && createPatternIfRelevant(rule, this::createFWDPattern, PatternType.FWD))
 						createFWD_GREENCORRPattern(rule);
+					else createPatternIfRelevant(rule, this::createFWDPattern, PatternType.FWD);
 					}
 				else createPatternIfRelevant(rule, this::createFWDPattern, PatternType.FWD);
 				
@@ -132,7 +133,7 @@ public class ContextPatternTransformation {
 			}
 
 			if (isDomainProgressive(rule, DomainType.TRG)) {
-				if(patternInvOption && isPatternRelevant(rule, PatternType.FWD)) {
+				if(patternInvOption && isPatternRelevant(rule, PatternType.BWD)) {
 					createTRGPattern(rule);
 				}				
 				createPatternIfRelevant(rule, this::createBWDPattern, PatternType.BWD);
