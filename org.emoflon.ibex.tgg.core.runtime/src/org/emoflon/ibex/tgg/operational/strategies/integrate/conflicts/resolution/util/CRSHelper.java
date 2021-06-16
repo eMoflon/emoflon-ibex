32dfbdaf9bf264c0b4712b8cca664798a7b95eec
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2021-06-16 23:09:26 +0200

    Add forEachConflict method in CRSHelper

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/strategies/integrate/conflicts/resolution/util/CRSHelper.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/strategies/integrate/conflicts/resolution/util/CRSHelper.java
@@ -8,12 +8,16 @@ import org.emoflon.ibex.tgg.operational.strategies.integrate.conflicts.resolutio
 
 public class CRSHelper {
 
+	public static void forEachConflict(ConflictContainer conflictContainer, Consumer<Conflict> action) {
+		for (Conflict conflict : conflictContainer.getConflicts())
+			action.accept(conflict);
+		for (ConflictContainer container : conflictContainer.getSubContainers())
+			forEachConflict(container, action);
+	}
+
 	public static <S extends ConflictResolutionStrategy, C extends S> void forEachResolve(ConflictContainer conflictContainer, //
 			Class<C> conflictClass, Consumer<S> crs) {
-		for (Conflict conflict : conflictContainer.getConflicts())
-			forConflict(conflict, conflictClass, crs);
-		for (ConflictContainer container : conflictContainer.getSubContainers())
-			forEachResolve(container, conflictClass, crs);
+		forEachConflict(conflictContainer, conflict -> forConflict(conflict, conflictClass, crs));
 	}
 
 	@SuppressWarnings("unchecked")
