49ab0e5af56a8912fd05898e4abe505fddcf797e
SebastianE <sebastian.ehmes@gmail.com>
2021-07-02 17:07:43 +0200

    fixed cases with no deletion or creation

--- org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GraphTransformationIterationInterpreter.java
+++ org.emoflon.ibex.gt/src/org/emoflon/ibex/gt/engine/GraphTransformationIterationInterpreter.java
@@ -45,92 +45,109 @@ public class GraphTransformationIterationInterpreter {
 		Map<EReference, Map<EObject, List<EObject>>> deletedContainments = new HashMap<>();
 		
 		for(IBeXForEachExpression iterator : iteratorPatterns) {
-			for(IBeXEdge edge : iterator.getCreate().getCreatedEdges()) {
-				EObject itrSrc = (EObject) match.get(iterator.getSource().getName());
-				EObject src = (EObject) match.get(edge.getSourceNode().getName());
-				EReference itrRef = iterator.getEdge().getType();
-				EReference ref = edge.getType();
-				if(itrRef.isMany()) {
-					// Make copy of list to prevent concurrent modification exceptions in case of containment edges
-					List<EObject> trgs = new LinkedList<>();
-					trgs.addAll((List<EObject>) itrSrc.eGet(itrRef));
-					for(EObject trg : trgs) {
+			if(iterator.getCreate() != null) {
+				for(IBeXEdge edge : iterator.getCreate().getCreatedEdges()) {
+					EObject itrSrc = (EObject) match.get(iterator.getSource().getName());
+					EObject src = (EObject) match.get(edge.getSourceNode().getName());
+					EReference itrRef = iterator.getEdge().getType();
+					EReference ref = edge.getType();
+					if(itrRef.isMany()) {
+						// Make copy of list to prevent concurrent modification exceptions in case of containment edges
+						List<EObject> trgs = new LinkedList<>();
+						trgs.addAll((List<EObject>) itrSrc.eGet(itrRef));
+						for(EObject trg : trgs) {
+							EMFManipulationUtils.createEdge(src, trg, ref);
+						}
+						
+						if(ref.isContainment()) {
+							Map<EObject, List<EObject>> containments = deletedContainments.get(ref);
+							if(containments == null) {
+								containments = new HashMap<>();
+								deletedContainments.put(ref, containments);
+							}
+							if(containments.containsKey(itrSrc)) {
+								containments.get(itrSrc).addAll(trgs);
+							} else {
+								containments.put(itrSrc, trgs);
+							}
+						}
+					}else {
+						EObject trg = (EObject) itrSrc.eGet(itrRef);
 						EMFManipulationUtils.createEdge(src, trg, ref);
-					}
-					
-					if(ref.isContainment()) {
-						Map<EObject, List<EObject>> containments = deletedContainments.get(ref);
-						if(containments == null) {
-							containments = new HashMap<>();
-							deletedContainments.put(ref, containments);
-						}
-						if(containments.containsKey(itrSrc)) {
-							containments.get(itrSrc).addAll(trgs);
-						} else {
-							containments.put(itrSrc, trgs);
-						}
-					}
-				}else {
-					EObject trg = (EObject) itrSrc.eGet(itrRef);
-					EMFManipulationUtils.createEdge(src, trg, ref);
-					
-					if(ref.isContainment()) {
-						Map<EObject, List<EObject>> containments = deletedContainments.get(ref);
-						if(containments == null) {
-							containments = new HashMap<>();
-							deletedContainments.put(ref, containments);
-						}
-						if(containments.containsKey(itrSrc)) {
-							containments.get(itrSrc).add(trg);
-						} else {
-							containments.put(itrSrc, new LinkedList<>());
-							containments.get(itrSrc).add(trg);
+						
+						if(ref.isContainment()) {
+							Map<EObject, List<EObject>> containments = deletedContainments.get(ref);
+							if(containments == null) {
+								containments = new HashMap<>();
+								deletedContainments.put(ref, containments);
+							}
+							if(containments.containsKey(itrSrc)) {
+								containments.get(itrSrc).add(trg);
+							} else {
+								containments.put(itrSrc, new LinkedList<>());
+								containments.get(itrSrc).add(trg);
+							}
 						}
 					}
 				}
-			}
-			
-			for(IBeXAttributeAssignment asgn : iterator.getCreate().getAttributeAssignments()) {
-				EObject itrSrc = (EObject) match.get(iterator.getSource().getName());
-				EReference itrRef = iterator.getEdge().getType();
-				if(itrRef.isMany()) {
-					List<EObject> trgs = null;
-					if(itrRef.isContainment() && deletedContainments.containsKey(itrRef) && deletedContainments.get(itrRef).containsKey(itrSrc)) {
-						trgs = deletedContainments.get(itrRef).get(itrSrc);
-					} else {
-						trgs = (List<EObject>) itrSrc.eGet(itrRef);
-					}
-					
-					for(EObject trg : trgs) {
+				
+				for(IBeXAttributeAssignment asgn : iterator.getCreate().getAttributeAssignments()) {
+					EObject itrSrc = (EObject) match.get(iterator.getSource().getName());
+					EReference itrRef = iterator.getEdge().getType();
+					if(itrRef.isMany()) {
+						List<EObject> trgs = null;
+						if(itrRef.isContainment() && deletedContainments.containsKey(itrRef) && deletedContainments.get(itrRef).containsKey(itrSrc)) {
+							trgs = deletedContainments.get(itrRef).get(itrSrc);
+						} else {
+							trgs = (List<EObject>) itrSrc.eGet(itrRef);
+						}
+						
+						for(EObject trg : trgs) {
+							// Calculate attribute values.
+							SimpleMatch dynamicMatch = new SimpleMatch(match);
+							dynamicMatch.put(iterator.getTrgIterator().getName(), trg);
+							AssignmentTriple assignment = GraphTransformationCreateInterpreter.calculateAssignment(asgn, dynamicMatch, parameters, contextInterpreter);
+							assignment.getObject().eSet(assignment.getAttribute(), assignment.getValue());
+						}
+					}else {
+						EObject trg = null;
+						if(itrRef.isContainment() && deletedContainments.containsKey(itrRef) && deletedContainments.get(itrRef).containsKey(itrSrc)) {
+							trg = deletedContainments.get(itrRef).get(itrSrc).get(0);
+						} else {
+							trg = (EObject) itrSrc.eGet(itrRef);
+						}
 						// Calculate attribute values.
 						SimpleMatch dynamicMatch = new SimpleMatch(match);
 						dynamicMatch.put(iterator.getTrgIterator().getName(), trg);
 						AssignmentTriple assignment = GraphTransformationCreateInterpreter.calculateAssignment(asgn, dynamicMatch, parameters, contextInterpreter);
 						assignment.getObject().eSet(assignment.getAttribute(), assignment.getValue());
 					}
-				}else {
-					EObject trg = null;
-					if(itrRef.isContainment() && deletedContainments.containsKey(itrRef) && deletedContainments.get(itrRef).containsKey(itrSrc)) {
-						trg = deletedContainments.get(itrRef).get(itrSrc).get(0);
-					} else {
-						trg = (EObject) itrSrc.eGet(itrRef);
-					}
-					// Calculate attribute values.
-					SimpleMatch dynamicMatch = new SimpleMatch(match);
-					dynamicMatch.put(iterator.getTrgIterator().getName(), trg);
-					AssignmentTriple assignment = GraphTransformationCreateInterpreter.calculateAssignment(asgn, dynamicMatch, parameters, contextInterpreter);
-					assignment.getObject().eSet(assignment.getAttribute(), assignment.getValue());
 				}
 			}
 			
-			for(IBeXEdge edge : iterator.getDelete().getDeletedEdges()) {
-				EObject itrSrc = (EObject) match.get(iterator.getSource().getName());
-				EObject src = (EObject) match.get(edge.getSourceNode().getName());
-				EReference itrRef = iterator.getEdge().getType();
-				EReference ref = edge.getType();
-				if(itrRef.isMany()) {
-					List<EObject> trgs = (List<EObject>) itrSrc.eGet(itrRef);
-					for(EObject trg : trgs) {
+			if(iterator.getDelete() != null) {
+				for(IBeXEdge edge : iterator.getDelete().getDeletedEdges()) {
+					EObject itrSrc = (EObject) match.get(iterator.getSource().getName());
+					EObject src = (EObject) match.get(edge.getSourceNode().getName());
+					EReference itrRef = iterator.getEdge().getType();
+					EReference ref = edge.getType();
+					if(itrRef.isMany()) {
+						List<EObject> trgs = (List<EObject>) itrSrc.eGet(itrRef);
+						for(EObject trg : trgs) {
+							Map<EObject, List<EObject>> src2deleted = deletedEdges.get(ref);
+							if(src2deleted == null) {
+								src2deleted = new HashMap<>();
+								deletedEdges.put(ref, src2deleted);
+							}
+							List<EObject> deleted = src2deleted.get(src);
+							if(deleted == null) {
+								deleted = new LinkedList<>();
+								src2deleted.put(src, deleted);
+							}
+							deleted.add(trg);
+						}
+					}else {
+						EObject trg = (EObject) itrSrc.eGet(itrRef);
 						Map<EObject, List<EObject>> src2deleted = deletedEdges.get(ref);
 						if(src2deleted == null) {
 							src2deleted = new HashMap<>();
@@ -143,21 +160,9 @@ public class GraphTransformationIterationInterpreter {
 						}
 						deleted.add(trg);
 					}
-				}else {
-					EObject trg = (EObject) itrSrc.eGet(itrRef);
-					Map<EObject, List<EObject>> src2deleted = deletedEdges.get(ref);
-					if(src2deleted == null) {
-						src2deleted = new HashMap<>();
-						deletedEdges.put(ref, src2deleted);
-					}
-					List<EObject> deleted = src2deleted.get(src);
-					if(deleted == null) {
-						deleted = new LinkedList<>();
-						src2deleted.put(src, deleted);
-					}
-					deleted.add(trg);
 				}
-			}
+			}	
+			
 		}
 		
 		for(EReference ref : deletedEdges.keySet()) {
