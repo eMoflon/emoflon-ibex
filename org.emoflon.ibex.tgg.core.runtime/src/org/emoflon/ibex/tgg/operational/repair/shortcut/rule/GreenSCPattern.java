009d1b69a79aeaeb7dabae8466a119147d34334c
Lars Fritsche <lars.fritsche@es.tu-darmstadt.de>
2021-08-08 13:33:58 +0200

    optimized csp evaluation for shortcut framework and standard propagation

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/repair/shortcut/rule/GreenSCPattern.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/repair/shortcut/rule/GreenSCPattern.java
@@ -2,17 +2,28 @@ package org.emoflon.ibex.tgg.operational.repair.shortcut.rule;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.emoflon.ibex.tgg.compiler.patterns.TGGPatternUtil;
 import org.emoflon.ibex.tgg.operational.csp.IRuntimeTGGAttrConstrContainer;
 import org.emoflon.ibex.tgg.operational.csp.RuntimeTGGAttributeConstraintContainer;
 import org.emoflon.ibex.tgg.operational.csp.sorting.SearchPlanAction;
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
 import org.emoflon.ibex.tgg.operational.matches.ITGGMatch;
+import org.emoflon.ibex.tgg.operational.patterns.IGreenPattern;
 import org.emoflon.ibex.tgg.operational.patterns.IGreenPatternFactory;
 import org.emoflon.ibex.tgg.operational.patterns.IbexGreenPattern;
 import org.emoflon.ibex.tgg.operational.repair.util.TGGFilterUtil;
+import org.emoflon.ibex.tgg.operational.strategies.modules.TGGResourceHandler;
+import org.emoflon.ibex.tgg.util.TGGModelUtils;
 
 import language.BindingType;
 import language.DomainType;
@@ -32,18 +43,42 @@ import language.TGGRuleNode;
  * @author lfritsche
  *
  */
-public class GreenSCPattern extends IbexGreenPattern {
+public class GreenSCPattern implements IGreenPattern {
 
 	private OperationalShortcutRule oscRule;
+	private IbexOptions options;
 	private IGreenPatternFactory factory;
+	private List<TGGParamValue> cspVariables;
+	private List<TGGAttributeConstraint> sortedAttributeConstraints;
+	private boolean optimizeMarkerCreation = false;
+	private Map<TGGRuleNode, EReference> name2ref = new HashMap<>();
+	private TGGResourceHandler resourceHandler;
 	
 	
 	public GreenSCPattern(IGreenPatternFactory factory, OperationalShortcutRule oscRule) {
-		super(factory);
 		this.oscRule = oscRule;
 		this.factory = factory;
+		options = factory.getOptions();
+		resourceHandler = options.resourceHandler();
+		optimizeMarkerCreation = options.blackInterpreter() != null && options.blackInterpreter().getClass().getName().contains("HiPE");
+		initialize(factory);
 	}
 	
+	private void initialize(IGreenPatternFactory factory) {
+		TGGAttributeConstraintLibrary newLibrary = EcoreUtil.copy(factory.getAttributeLibrary());
+		
+		cspVariables = newLibrary.getParameterValues();
+		List<TGGAttributeConstraint> newAttributeConstraints = newLibrary.getTggAttributeConstraints();
+		sortedAttributeConstraints = sortConstraints(cspVariables, newAttributeConstraints);
+		for(TGGParamValue pVal : cspVariables) {
+			if(pVal instanceof TGGAttributeExpression) {
+				TGGAttributeExpression attrExpr = (TGGAttributeExpression) pVal;
+				attrExpr.setObjectVar(oscRule.opScRule.mapReplacingToSCNodeNode(attrExpr.getObjectVar().getName()));
+			}
+		}
+	}
+
+
 	@Override
 	public Collection<TGGRuleNode> getSrcNodes() {
 		return TGGFilterUtil.filterNodes(oscRule.getOpScRule().getNodes(), DomainType.SRC, BindingType.CREATE);
@@ -86,22 +121,9 @@ public class GreenSCPattern extends IbexGreenPattern {
 
 	@Override
 	public IRuntimeTGGAttrConstrContainer getAttributeConstraintContainer(ITGGMatch match) {
-//		try {
-			TGGAttributeConstraintLibrary newLibrary = EcoreUtil.copy(factory.getAttributeLibrary());
-			
-			List<TGGParamValue> newAttributeCSPVariables = newLibrary.getParameterValues();
-			List<TGGAttributeConstraint> newAttributeConstraints = newLibrary.getTggAttributeConstraints();
-			List<TGGAttributeConstraint> sortedConstraints = sortConstraints(newAttributeCSPVariables, newAttributeConstraints);
-			for(TGGParamValue pVal : newAttributeCSPVariables) {
-				if(pVal instanceof TGGAttributeExpression) {
-					TGGAttributeExpression attrExpr = (TGGAttributeExpression) pVal;
-					attrExpr.setObjectVar(oscRule.opScRule.mapReplacingToSCNodeNode(attrExpr.getObjectVar().getName()));
-				}
-			}
-			
 			return new RuntimeTGGAttributeConstraintContainer(
-					newAttributeCSPVariables, 
-					sortedConstraints,
+					cspVariables, 
+					sortedAttributeConstraints,
 					match,
 					factory.getOptions().csp.constraintProvider());
 	}
@@ -124,5 +146,94 @@ public class GreenSCPattern extends IbexGreenPattern {
 	@Override
 	public Collection<TGGRuleEdge> getCorrEdges() {
 		return TGGFilterUtil.filterEdges(oscRule.getOpScRule().getEdges(), DomainType.CORR, BindingType.CREATE);
+	}
+
+	@Override
+	public void createMarkers(String ruleName, ITGGMatch match) {
+		EPackage corrPackage = options.tgg.corrMetamodel();
+		EClass type = (EClass) corrPackage.getEClassifier(TGGModelUtils.getMarkerTypeName(ruleName));
+		
+		EObject ra = EcoreUtil.create(type);
+		
+		if(!optimizeMarkerCreation) {
+			resourceHandler.getProtocolResource().getContents().add(ra);
+		}
+	
+		for (TGGRuleNode n : factory.getGreenSrcNodesInRule()) {
+			EReference ref;
+			if(name2ref.containsKey(n))
+				ref = name2ref.get(n);
+			else {
+				String refName = TGGModelUtils.getMarkerRefName(BindingType.CREATE, DomainType.SRC, n.getName());
+				ref = (EReference) type.getEStructuralFeature(refName);		
+				name2ref.put(n, ref);
+			}
+			ra.eSet(ref, (EObject) match.get(n.getName()));			
+		}
+		
+		for (TGGRuleNode n : factory.getBlackSrcNodesInRule()) {
+			EReference ref;
+			if(name2ref.containsKey(n))
+				ref = name2ref.get(n);
+			else {
+				String refName = TGGModelUtils.getMarkerRefName(BindingType.CONTEXT, DomainType.SRC, n.getName());
+				ref = (EReference) type.getEStructuralFeature(refName);		
+				name2ref.put(n, ref);
+			}		
+			ra.eSet(ref, (EObject) match.get(n.getName()));			
+		}
+
+		for (TGGRuleNode n : factory.getGreenTrgNodesInRule()) {
+			EReference ref;
+			if(name2ref.containsKey(n))
+				ref = name2ref.get(n);
+			else {
+				String refName = TGGModelUtils.getMarkerRefName(BindingType.CREATE, DomainType.TRG, n.getName());
+				ref = (EReference) type.getEStructuralFeature(refName);		
+				name2ref.put(n, ref);
+			}		
+			ra.eSet(ref, (EObject) match.get(n.getName()));			
+		}
+		
+		for (TGGRuleNode n : factory.getBlackTrgNodesInRule()) {
+			EReference ref;
+			if(name2ref.containsKey(n))
+				ref = name2ref.get(n);
+			else {
+				String refName = TGGModelUtils.getMarkerRefName(BindingType.CONTEXT, DomainType.TRG, n.getName());
+				ref = (EReference) type.getEStructuralFeature(refName);		
+				name2ref.put(n, ref);
+			}		
+			ra.eSet(ref, (EObject) match.get(n.getName()));			
+		}
+
+		for (TGGRuleNode n : factory.getGreenCorrNodesInRule()) {
+			EReference ref;
+			if(name2ref.containsKey(n))
+				ref = name2ref.get(n);
+			else {
+				String refName = TGGModelUtils.getMarkerRefName(BindingType.CREATE, DomainType.CORR, n.getName());
+				ref = (EReference) type.getEStructuralFeature(refName);		
+				name2ref.put(n, ref);
+			}		
+			ra.eSet(ref, (EObject) match.get(n.getName()));			
+		}
+		
+		for (TGGRuleNode n : factory.getBlackCorrNodesInRule()) {
+			EReference ref;
+			if(name2ref.containsKey(n))
+				ref = name2ref.get(n);
+			else {
+				String refName = TGGModelUtils.getMarkerRefName(BindingType.CONTEXT, DomainType.CORR, n.getName());
+				ref = (EReference) type.getEStructuralFeature(refName);		
+				name2ref.put(n, ref);
+			}	
+			ra.eSet(ref, (EObject) match.get(n.getName()));			
+		}
+		
+		if(optimizeMarkerCreation) {
+			resourceHandler.getProtocolResource().getContents().add(ra);
+		}
+		match.put(TGGPatternUtil.getProtocolNodeName(ruleName), ra);
 	}
 }
