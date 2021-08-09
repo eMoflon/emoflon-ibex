e575f87c08bcc58bf3da747a30db958c9b260756
Lars Fritsche <lars.fritsche@es.tu-darmstadt.de>
2021-08-09 20:50:54 +0200

    replaced javabdd with a silent version without system.outs that CANNOT BE DISABLED also removed the hacky solution to deactivate prints on the console while javabdd was running

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/csp/sorting/SearchPlanAction.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/csp/sorting/SearchPlanAction.java
@@ -62,20 +62,6 @@ public class SearchPlanAction extends Algorithm<SimpleCombiner, TGGAttributeCons
 		List<WeightedOperation<TGGAttributeConstraint>> weightedOperations = createWeightedOperations(constraints);
 
 		// 3. Call search plan algorithm to sort weighted operations
-
-		PrintStream out = System.out;
-		PrintStream err = System.err;
-		System.setOut(new PrintStream(new OutputStream() {
-			@Override
-			public void write(int b) throws IOException {
-			}
-		}));
-		System.setErr(new PrintStream(new OutputStream() {
-			@Override
-			public void write(int b) throws IOException {
-			}
-		}));
-
 		try {
 			SimpleCombiner sc = generatePlan(new SimpleCombiner(), weightedOperations, inputAdornment);
 			List<TGGAttributeConstraint> sortedList = new ArrayList<>();
@@ -89,10 +75,7 @@ public class SearchPlanAction extends Algorithm<SimpleCombiner, TGGAttributeCons
 			throw new IllegalStateException(
 					constraints.stream().map(c -> c.getDefinition().getName()).collect(Collectors.toList()) + ", "
 							+ e.getMessage());
-		} finally {
-			System.setOut(out);
-			System.setErr(err);
-		}
+		} 
 	}
 
 	private Adornment determineInputAdornment() {
