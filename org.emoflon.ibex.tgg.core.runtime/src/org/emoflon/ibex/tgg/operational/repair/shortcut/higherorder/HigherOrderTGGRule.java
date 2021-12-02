cf9476b5b49fbb3f19804ceadb77aaa989b9078d
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2021-12-02 23:18:12 +0100

    Implementation: structure of HigherOrderTGGRule

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/repair/shortcut/higherorder/HigherOrderTGGRule.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/operational/repair/shortcut/higherorder/HigherOrderTGGRule.java
@@ -1,33 +1,59 @@
 package org.emoflon.ibex.tgg.operational.repair.shortcut.higherorder;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
+
+import org.emoflon.ibex.tgg.util.ConsoleUtil;
 
 import language.TGGRule;
 import language.TGGRuleNode;
 
 public class HigherOrderTGGRule {
 
-	private List<TGGRule> rules;
-	private List<Map<TGGRuleNode, TGGRuleNode>> contextMappings;
+	private final LinkedList<HigherOrderRuleComponent> components;
 
 	public HigherOrderTGGRule() {
-		this.rules = new LinkedList<>();
-		this.contextMappings = new LinkedList<>();
+		this.components = new LinkedList<>();
 	}
 
-	public void addRule(TGGRule rule, Map<TGGRuleNode, TGGRuleNode> contextMapping) {
-		rules.add(rule);
-		contextMappings.add(contextMapping);
+	public void addComponentFirst(TGGRule rule, Map<TGGRuleNode, ComponentSpecificRuleNode> contextMapping) {
+		components.addFirst(new HigherOrderRuleComponent(rule, contextMapping));
 	}
 
-	public List<TGGRule> getRules() {
-		return rules;
+	public void addComponentLast(TGGRule rule, Map<TGGRuleNode, ComponentSpecificRuleNode> contextMapping) {
+		components.addLast(new HigherOrderRuleComponent(rule, contextMapping));
 	}
 
-	public List<Map<TGGRuleNode, TGGRuleNode>> getContextMappings() {
-		return contextMappings;
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("HigherOrderTGGRule [\n");
+		for (HigherOrderRuleComponent component : components) {
+			builder.append(ConsoleUtil.indent(component.rule.getName(), 2, true));
+			builder.append("\n");
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public class HigherOrderRuleComponent {
+		public final TGGRule rule;
+		public final Map<TGGRuleNode, ComponentSpecificRuleNode> contextMapping;
+
+		private HigherOrderRuleComponent(TGGRule rule, Map<TGGRuleNode, ComponentSpecificRuleNode> contextMapping) {
+			this.rule = rule;
+			this.contextMapping = contextMapping;
+		}
+	}
+
+	public class ComponentSpecificRuleNode {
+		public final TGGRuleNode ruleNode;
+		public final HigherOrderRuleComponent component;
+
+		public ComponentSpecificRuleNode(TGGRuleNode ruleNode, HigherOrderRuleComponent component) {
+			this.ruleNode = ruleNode;
+			this.component = component;
+		}
 	}
 
 }
