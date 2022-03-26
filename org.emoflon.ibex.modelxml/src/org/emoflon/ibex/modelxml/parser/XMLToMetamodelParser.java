0a115920f65bb2c200b90dd802b8cc0cf41b2ef5
Lars <Lars.heublein@web.de>
2022-03-26 16:11:06 +0100

    reworked parser to cover nested attributes

--- org.emoflon.ibex.modelxml/src/org/emoflon/ibex/modelxml/parser/XMLToMetamodelParser.java
+++ org.emoflon.ibex.modelxml/src/org/emoflon/ibex/modelxml/parser/XMLToMetamodelParser.java
@@ -3,17 +3,21 @@ package org.emoflon.ibex.modelxml.parser;
 import java.io.IOException;
 import java.util.*;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.*;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.jdom2.Attribute;
 import org.jdom2.Element;
 
 public class XMLToMetamodelParser extends ParserUtil {
 	public static void main(String[] args) {
-		parseXMLFileToMetaModel("misc/test1.xml");
+		parseXMLFileToMetaModel("misc/output.xml");
 	}
-
+	
+	static Map<String, EClass> classMap = new HashMap<>();
+	
 	public static EPackage parseXMLFileToMetaModel(String fileName) {
 		var end = fileName.lastIndexOf(".");
 		var fileNameWoEnd = fileName.substring(0, end);
@@ -26,17 +30,18 @@ public class XMLToMetamodelParser extends ParserUtil {
 		var r = rs.createResource(URI.createURI(fileNameWoEnd + ".ecore"));
 		var container = EcoreFactory.eINSTANCE.createEPackage();
 
-		rs.getURIConverter().getURIMap().put(
-				URI.createPlatformResourceURI(fileName, true),
-				URI.createURI(fileName));
+//		rs.getURIConverter().getURIMap().put(
+//				URI.createPlatformResourceURI(fileName, true),
+//				URI.createURI(fileName));
 		container.setName(fileNameWoEnd.substring(fileNameWoEnd.indexOf("/") +1));
 		container.setNsPrefix(fileNameWoEnd.substring(fileNameWoEnd.indexOf("/") +1));
 		container.setNsURI("platform:/resource/org.emoflon.ibex.modelxml/" + fileNameWoEnd + ".ecore");
-		EClass clss = parseNodeMeta(rootElement, container);
 		
-		for(var cl : clss.getEReferences())
-			System.out.println(cl.getName() + " ref: " + cl.getEReferenceType().getName());
-		container.getEClassifiers().add(clss);
+		parseNodeMeta(rootElement, "", null);
+		
+		for (var clss : classMap.entrySet()) {
+			container.getEClassifiers().add(clss.getValue());
+		}
 
 		r.getContents().add(container);
 		try {
@@ -47,119 +52,67 @@ public class XMLToMetamodelParser extends ParserUtil {
 		return container;
 	}
 
-	private static EClass parseNodeMeta(Element element, EPackage container) {
+	private static void parseNodeMeta(Element element, String name, EClass parent) {
+		name += element.getName();
+		if (classMap.containsKey(name)) {
+			// add to existing
+			EClass eclass = classMap.get(name);
+			var echild = eclass;
+			/** check attr
+			 *  check its children
+			 */
+			var features = echild.getEStructuralFeatures();
+			var attributes = features.stream().filter(e -> e instanceof EAttribute).map(e -> {
+				var a = (EAttribute) e;
+				return a.getName() + a.getEAttributeType().getName();
+			}).toList();
+			for (var att: element.getAttributes()) {
+				var a = att.getName() + getDataType(att.getValue()).getName();
+				if (!attributes.contains(a)) {
+					addAttribute(features, att);
+				}
+			}
+			for (var c: element.getChildren()) {
+				parseNodeMeta(c, name, eclass);
+			}
+			return;
+		} 
+		
+		// new class found
 		var factory = EcorePackage.eINSTANCE.getEcoreFactory();
 		var eclass = factory.createEClass();
 		eclass.setName(element.getName());
-
-		var eAttributes = eclass.getEStructuralFeatures();
+		
+		var<EAttribute> eAttributes = eclass.getEStructuralFeatures();
 		for (var att : element.getAttributes()) {
-			var a = factory.createEAttribute();
-			a.setName(att.getName());
-			a.setEType(getDataType(att.getValue()));
-			eAttributes.add(a);
+			addAttribute(eAttributes, att);
 		}
 
-		// map children by their name into lists
-		var childMap = new HashMap<String, List<EClass>>();
-		mapChildren(element, container, childMap);
-
-		// merge children with the same name
-		eclass = saveMergedChildren(container, eclass, childMap);
-//		System.out.println("-----------" +eclass.getName());
-//		for(var cl : eclass.getEReferences())
-//			System.out.println(cl.getName() + cl.getEReferenceType().toString());
-		return eclass;
-	}
-
-	/**
-	 * This method saves the merged children into container. Furthermore, it creates
-	 * a references from eclass to all merged children.
-	 * 
-	 * @param container Stores the Metamodel
-	 * @param eclass    Representation of the current parsed node.
-	 * @param childMap  memory for all children sorted by their names
-	 * @return
-	 */
-	private static EClass saveMergedChildren(EPackage container, EClass eclass,
-			HashMap<String, List<EClass>> childMap) {
-		for (var c : childMap.entrySet()) {
-			var mergedClass = mergeNodes(c.getValue(), container);
-			container.getEClassifiers().add(mergedClass);
-			// TODO: set containment for mergedClass to eclass
-			var eref = EcoreFactory.eINSTANCE.createEReference();
-			eref.setName(mergedClass.getName().toLowerCase());
-			eref.setContainment(true);
-			eref.setEType(mergedClass);
-			eclass.getEStructuralFeatures().add(eref);
-		}
-		return eclass;
-	}
-
-	/**
-	 * This method put children in a map, where children a sorted by their names
-	 * 
-	 * @param element   Contains the element, which children should be parsed into
-	 *                  eclasses
-	 * @param container It contains the metamodel objects
-	 * @param childMap  Store all parsed children, while store tupel consisting of
-	 *                  equal-named classes and their names
-	 */
-	private static void mapChildren(Element element, EPackage container, HashMap<String, List<EClass>> childMap) {
 		for (var child : element.getChildren()) {
-			var eChild = parseNodeMeta(child, container);
-//			System.out.println(eChild.getName());
-			if (childMap.containsKey(child.getName())) {
-				var entry = childMap.get(child.getName());
-				entry.add(eChild);
-			} else {
-				var entry = new ArrayList<EClass>();
-				entry.add(eChild);
-				childMap.put(child.getName(), entry);
-			}
-		}
+			parseNodeMeta(child, name, eclass);
+		}		
+		classMap.put(name, eclass);
+		if (parent != null)
+			createReference(parent, eclass);
 	}
 
-	/**
-	 * All elements in nodes have the same name - they are instances of one generic
-	 * class
-	 * 
-	 * @param nodes
-	 * @return
-	 */
-	private static EClass mergeNodes(List<EClass> nodes, EPackage container) {
+	private static void addAttribute(EList<EStructuralFeature> features, Attribute att) {
 		var factory = EcorePackage.eINSTANCE.getEcoreFactory();
-		var eclass = factory.createEClass();
-		eclass.setName(nodes.get(0).getName());
-
-		var attributes = new HashMap<String, EAttribute>();
-		var references = new HashMap<String, EReference>();
-		for (var ch : nodes) {
-			var mem = ch.getEStructuralFeatures();
-			var attributesNode = mem.stream().filter(e -> e instanceof EAttribute).map(e -> (EAttribute) e).toList();
-			for (var attr : attributesNode) {
-				if (!attributes.containsKey(attr.getName())) {
-					attributes.put(attr.getName(), attr);
-				}
-			}
-			var refFeatures = mem.stream().filter(e -> e instanceof EReference).map(e -> (EReference) e).toList();
-			for(var ref : refFeatures) {
-				if(!references.containsKey(ref.getName())) {
-					references.put(ref.getName(), ref);
-				}
-			}
-		}
-		for (var att : attributes.entrySet()) {
-			eclass.getEStructuralFeatures().add(att.getValue());
-		}
-		for(var ref : references.entrySet()) {
-			eclass.getEStructuralFeatures().add(ref.getValue());
-		}
-
-		container.getEClassifiers().add(eclass);
-
-		return eclass;
+		var eAttr = factory.createEAttribute();
+		eAttr.setName(att.getName());
+		eAttr.setEType(getDataType(att.getValue()));
+		features.add(eAttr);
 	}
+
+
+	private static void createReference(EClass parent, EClass child) {
+		var eref = EcoreFactory.eINSTANCE.createEReference();
+		eref.setName(child.getName().toLowerCase());
+		eref.setContainment(true);
+		eref.setEType(child);
+		parent.getEStructuralFeatures().add(eref);
+	}
+
 
 	private static EDataType getDataType(String value) {
 		try {
