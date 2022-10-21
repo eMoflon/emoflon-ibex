293035a5b6b969fa97a5df66970363c79b7eab5f
Lars Fritsche <arikae00@jesitec.de>
2022-10-21 10:19:28 +0200

    + further work on tgg transformer

--- org.emoflon.ibex.tgg.compiler/src/org/emoflon/ibex/tgg/compiler/TGGLToTGGModelTransformer.java
+++ org.emoflon.ibex.tgg.compiler/src/org/emoflon/ibex/tgg/compiler/TGGLToTGGModelTransformer.java
@@ -1,5 +1,7 @@
 package org.emoflon.ibex.tgg.compiler;
 
+import static org.emoflon.ibex.common.slimgt.util.SlimGTModelUtil.getContainer;
+
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Comparator;
@@ -11,13 +13,17 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.emoflon.ibex.common.coremodel.IBeXCoreModel.EPackageDependency;
+import org.emoflon.ibex.common.coremodel.IBeXCoreModel.IBeXEdge;
 import org.emoflon.ibex.common.coremodel.IBeXCoreModel.IBeXModelMetadata;
 import org.emoflon.ibex.common.coremodel.IBeXCoreModel.IBeXNamedElement;
 import org.emoflon.ibex.common.coremodel.IBeXCoreModel.IBeXNode;
 import org.emoflon.ibex.common.coremodel.IBeXCoreModel.IBeXOperationType;
 import org.emoflon.ibex.common.coremodel.IBeXCoreModel.IBeXPattern;
+import org.emoflon.ibex.common.slimgt.slimGT.SlimRuleEdgeContext;
+import org.emoflon.ibex.common.slimgt.slimGT.SlimRuleEdgeCreation;
 import org.emoflon.ibex.common.slimgt.slimGT.SlimRuleNodeContext;
 import org.emoflon.ibex.common.slimgt.slimGT.SlimRuleNodeCreation;
 import org.emoflon.ibex.common.slimgt.util.SlimGTEMFUtil;
@@ -26,6 +32,7 @@ import org.emoflon.ibex.tgg.tggl.scoping.TGGLScopeProvider;
 import org.emoflon.ibex.tgg.tggl.tGGL.EditorFile;
 import org.emoflon.ibex.tgg.tggl.tGGL.SlimRule;
 import org.emoflon.ibex.tgg.tggl.tGGL.SlimRuleNode;
+import org.emoflon.ibex.tgg.tggl.tGGL.TGGCorrRule;
 import org.emoflon.ibex.tgg.tggl.tGGL.TGGCorrespondenceNode;
 import org.emoflon.ibex.tgg.tggl.tGGL.TGGCorrespondenceNodeContext;
 import org.emoflon.ibex.tgg.tggl.tGGL.TGGCorrespondenceNodeCreation;
@@ -34,15 +41,18 @@ import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.BindingType;
 import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.DomainType;
 import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.IBeXTGGModelFactory;
 import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.TGGCorrespondence;
+import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.TGGEdge;
 import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.TGGModel;
 import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.TGGNode;
 import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.TGGRule;
 
-import static org.emoflon.ibex.common.slimgt.util.SlimGTModelUtil.*;
+record EdgeSignature(EObject source, EObject target, EReference type) {
+	
+}
 
 public class TGGLToTGGModelTransformer extends SlimGtToIBeXCoreTransformer<EditorFile, TGGModel, IBeXTGGModelFactory> {
 	
-	private Map<EObject, EObject> tggl2tggModel = new ConcurrentHashMap<>();
+	private Map<Object, EObject> tggl2tggModel = new ConcurrentHashMap<>();
 	private Map<SlimRule, Collection<IBeXPattern>> pattern2ibexPatterns = new ConcurrentHashMap<>();
 	
 	public TGGLToTGGModelTransformer(EditorFile editorFile, final IProject project) {
@@ -140,6 +150,8 @@ public class TGGLToTGGModelTransformer extends SlimGtToIBeXCoreTransformer<Edito
 			internalRule.getNodes().add(transformTGGNode((SlimRuleNode) node.getCreation(), BindingType.CREATE, DomainType.TARGET));
 		}
 		
+		
+		
 		return internalRule;
 	}
 	
@@ -159,30 +171,95 @@ public class TGGLToTGGModelTransformer extends SlimGtToIBeXCoreTransformer<Edito
 				default -> IBeXOperationType.CONTEXT;
 				}
 		);
-		corrNode.setSource(transformTGGNode(node.getSource(), getBindingType(node.getSource()), DomainType.SOURCE));
-		corrNode.setTarget(transformTGGNode(node.getTarget(), getBindingType(node.getTarget()), DomainType.TARGET));
+		
+		var source = transformTGGNode(node.getSource(), getBindingType(node.getSource()), DomainType.SOURCE);
+		var target = transformTGGNode(node.getTarget(), getBindingType(node.getTarget()), DomainType.TARGET);
+		
+		corrNode.setSource(source);
+		corrNode.setTarget(target);
+		
+		transformTGGEdge(new EdgeSignature(corrNode, source, null), binding, domain);
+		transformTGGEdge(new EdgeSignature(corrNode, target, null), binding, domain);
 		
 		return corrNode;
 	}
 	
-	private BindingType getBindingType(SlimRuleNode node) {
-		var context = getContainer(node, SlimRuleNodeContext.class);
-		var creation = getContainer(node, SlimRuleNodeCreation.class);
+	private TGGEdge transformTGGEdge(EdgeSignature edgeSignature, BindingType binding, DomainType domain) {
+		if(tggl2tggModel.containsKey(edgeSignature))
+			return (TGGEdge) tggl2tggModel.get(edgeSignature);
 		
-		var corrContext = getContainer(node, TGGCorrespondenceNodeContext.class);
-		var corrCreation = getContainer(node, TGGCorrespondenceNodeCreation.class);
+		var edge = factory.createTGGEdge();
+		edge.setOperationType(switch(binding) {
+				case CREATE -> IBeXOperationType.CREATION;
+				case DELETE -> IBeXOperationType.DELETION;
+				default -> IBeXOperationType.CONTEXT;
+				}
+		);
+		edge.setType(edgeSignature.type());
+		edge.setBindingType(binding);
+		edge.setDomainType(domain);
+		
+		var source = transformNode(edgeSignature.source(), getBindingType(edgeSignature.source()), getDomainType(edgeSignature.source()));
+		var target = transformNode(edgeSignature.target(), getBindingType(edgeSignature.target()), getDomainType(edgeSignature.target()));
+		edge.setName(source.getName() + " -" + edgeSignature.type().getName()+"-> " + target.getName());
+		
+		return edge;
+	}
+	
+	private IBeXNode transformNode(EObject input, BindingType bindingType, DomainType domainType) {
+		if(input instanceof TGGCorrespondenceNode corrNode)
+			return transformTGGCorrespondenceNode(corrNode, bindingType, domainType);
+		
+		if(input instanceof SlimRuleNode slimNode) 
+			return transformTGGNode(slimNode, bindingType, domainType);
+		
+		throw new RuntimeException("Element could not be transformed to an ibex node: " + input );
+	}
 
+	private BindingType getBindingType(EObject obj) {
+		var contextEdge = getContainer(obj, SlimRuleEdgeContext.class);
+		var creationEdge = getContainer(obj, SlimRuleEdgeCreation.class);
+		
+		var context = getContainer(obj, SlimRuleNodeContext.class);
+		var creation = getContainer(obj, SlimRuleNodeCreation.class);
+		
+		var corrContext = getContainer(obj, TGGCorrespondenceNodeContext.class);
+		var corrCreation = getContainer(obj, TGGCorrespondenceNodeCreation.class);
+
+		if(contextEdge != null)
+			return BindingType.CONTEXT;
 		if(corrContext != null)
 			return BindingType.CONTEXT;
 		if(context != null)
 			return BindingType.CONTEXT;
 
+		if(creationEdge != null)
+			return BindingType.CREATE;
 		if(corrCreation != null)
 			return BindingType.CREATE;
 		if(creation != null)
 			return BindingType.CREATE;
 		
-		throw new RuntimeException("Cannot determine bindingtype for " + node);
+		throw new RuntimeException("Cannot determine bindingtype for " + obj);
+	}
+	
+	public static DomainType getDomainType(EObject context) {
+		var tggRule = getContainer(context, org.emoflon.ibex.tgg.tggl.tGGL.TGGRule.class);
+		var rule = getContainer(context, TGGDomainRule.class);
+		if (rule != null) {
+			if (tggRule.getSourceRule().equals(rule))
+				return DomainType.SOURCE;
+			if (tggRule.getTargetRule().equals(rule))
+				return DomainType.TARGET;
+		}
+
+		// else it must be a correspondence node
+		var corrRule = getContainer(context, TGGCorrRule.class);
+		if (corrRule != null) {
+			return DomainType.CORRESPONDENCE;
+		}
+
+		throw new RuntimeException("Could not identify domain of element " + context);
 	}
 	
 	protected TGGNode transformTGGNode(SlimRuleNode node, BindingType binding, DomainType domain) {
