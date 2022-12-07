20a36d9ca31de91b6c580befb2853530464de924
Lars Fritsche <arikae00@jesitec.de>
2022-12-07 10:36:01 +0100

    moved jars to tgg.util

--- org.emoflon.ibex.util/src/org/emoflon/ibex/tgg/util/TGGEdgeUtil.java
+++ org.emoflon.ibex.util/src/org/emoflon/ibex/tgg/util/TGGEdgeUtil.java
@@ -3,13 +3,12 @@ package org.emoflon.ibex.tgg.util;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.emoflon.ibex.common.emf.EMFEdge;
-import org.emoflon.ibex.tgg.operational.matches.ITGGMatch;
+import org.emoflon.ibex.common.engine.SimpleMatch;
 import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.TGGEdge;
 
-import language.TGGRuleEdge;
 
 public class TGGEdgeUtil {
-	public static EMFEdge getRuntimeEdge(ITGGMatch match, TGGEdge specificationEdge) {
+	public static EMFEdge getRuntimeEdge(SimpleMatch match, TGGEdge specificationEdge) {
 		EObject src = (EObject) match.get(specificationEdge.getSource().getName());
 		EObject trg = (EObject) match.get(specificationEdge.getTarget().getName());
 		EReference ref = specificationEdge.getType();
