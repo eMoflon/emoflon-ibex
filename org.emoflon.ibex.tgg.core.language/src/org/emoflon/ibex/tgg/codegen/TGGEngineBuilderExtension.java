e4260ed4997ab12f9d2f0375afdeadda9a9054d7
Lars Fritsche <arikae00@jesitec.de>
2023-03-16 19:31:07 +0100

    changed builder extension and removed flattened model

--- org.emoflon.ibex.tgg.core.language/src/org/emoflon/ibex/tgg/codegen/TGGEngineBuilderExtension.java
+++ org.emoflon.ibex.tgg.core.language/src/org/emoflon/ibex/tgg/codegen/TGGEngineBuilderExtension.java
@@ -7,6 +7,6 @@ public interface TGGEngineBuilderExtension {
 	
 	static final String BUILDER_EXTENSON_ID = "org.emoflon.ibex.tgg.language.TGGEngineBuilderExtension";
 
-	void run(IProject project, EditorFile editorModel, EditorFile flattenedEditorModel);
+	void run(IProject project, EditorFile editorModel);
 
 }
