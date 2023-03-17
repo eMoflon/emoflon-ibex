da6f579e2f93faa846572e714685722c7a13d819
Lars Fritsche <arikae00@jesitec.de>
2023-03-17 23:29:14 +0100

    fixed tgg extension

--- org.emoflon.ibex.tgg.core.language/src/org/emoflon/ibex/tgg/codegen/TGGEngineBuilderExtension.java
+++ org.emoflon.ibex.tgg.core.language/src/org/emoflon/ibex/tgg/codegen/TGGEngineBuilderExtension.java
@@ -1,12 +1,12 @@
 package org.emoflon.ibex.tgg.codegen;
 
 import org.eclipse.core.resources.IProject;
-import org.emoflon.ibex.tgg.tggl.tGGL.EditorFile;
+import org.emoflon.ibex.tgg.tggmodel.IBeXTGGModel.TGGModel;
 
 public interface TGGEngineBuilderExtension {
 	
 	static final String BUILDER_EXTENSON_ID = "org.emoflon.ibex.tgg.language.TGGEngineBuilderExtension";
 
-	void run(IProject project, EditorFile editorModel);
+	void run(IProject project, TGGModel model);
 
 }
