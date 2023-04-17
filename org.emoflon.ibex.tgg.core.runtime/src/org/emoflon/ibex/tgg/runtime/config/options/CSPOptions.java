8e09ce2e13bbd933d46fb283986ac0fafcaaa929
Lars Fritsche <arikae00@jesitec.de>
2023-04-17 22:55:16 +0200

    fixed csp initialization

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/runtime/config/options/CSPOptions.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/runtime/config/options/CSPOptions.java
@@ -1,17 +1,26 @@
 package org.emoflon.ibex.tgg.runtime.config.options;
 
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.emoflon.ibex.tgg.runtime.csp.constraints.factories.RuntimeTGGAttrConstraintFactory;
 import org.emoflon.ibex.tgg.runtime.csp.constraints.factories.RuntimeTGGAttrConstraintProvider;
 
 public class CSPOptions extends IbexSubOptions {
 	
 	private RuntimeTGGAttrConstraintProvider constraintProvider;
-
+	private Collection<RuntimeTGGAttrConstraintFactory> factoryCache = new HashSet<>();
+	
 	public CSPOptions(IbexOptions options) {
 		super(options);
 	}
 	
 	public IbexOptions constraintProvider(RuntimeTGGAttrConstraintProvider constraintProvider) {
 		this.constraintProvider = constraintProvider;
+		if(constraintProvider != null) {
+			registerConstraintFactories(factoryCache);
+			factoryCache.clear();
+		}
 		return options;
 	}
 
@@ -19,4 +28,16 @@ public class CSPOptions extends IbexSubOptions {
 		return constraintProvider;
 	}
 
+	public void registerConstraintFactories(Collection<RuntimeTGGAttrConstraintFactory> factories) {
+		factories.forEach(this::registerConstraintFactory);
+	}
+	
+	public void registerConstraintFactory(RuntimeTGGAttrConstraintFactory factory) {
+		if(constraintProvider != null) {
+			constraintProvider.registerFactory(factory);
+		}
+		else {
+			factoryCache.add(factory);
+		}
+	}
 }
